af03bdda12a3579ed471e97902556b045bf00f22
Jeremy Freeman <freemanj11@janelia.hhmi.org>
2013-06-28 01:01:36 -0400

    Copies numpy and scipy to workers

--- helper/copy-numpy-scipy-ec2.sh
+++ helper/copy-numpy-scipy-ec2.sh
@@ -3,10 +3,10 @@
 SLAVES=`cat /root/spark-ec2/slaves`
 
 echo "installing numpy on master"
-sudo yum -y -q install numpy
+sudo yum -y -q install numpy scipy
 
 echo "installing numpy on slaves"
 for slave in $SLAVES; do
     echo "installing numpy on $slave"
-    ssh -t -t $slave sudo yum -y -q install numpy
+    ssh -t -t $slave sudo yum -y -q install numpy scipy
 done
