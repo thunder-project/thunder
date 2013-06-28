652c982eb24cfa1f7af6da198039e5f7243a46d9
Jeremy Freeman <freemanj11@janelia.hhmi.org>
2013-06-28 19:38:12 -0400

    Switched to iterative writing of results (to avoid large collection)

--- ica.py
+++ ica.py
@@ -1,14 +1,13 @@
 # ica <master> <inputFile> <outputFile_Components> <outputFile_Weights>
 # 
-# Perform ICA on a data matrix.
-# Input is a local text file or a file in HDFS.
-# Format should be rows of ' ' separated values.
-# - Example: space (rows) x time (cols).
-# - Rows should be whichever dim is larger.
-# Returns independent components and unmixing matrix in separate files
-
-# k is number of PCs to use in initial dimensionality reduction
-# c is the number of ICA components to return
+# perform ica on a data matrix.
+# input is a local text file or a file in HDFS
+# format should be rows of ' ' separated values
+# - example: space (rows) x time (cols)
+# - rows should be whichever dim is larger
+# 'k' is number of principal components to use in initial dim reduction
+# 'c' is the number of ica components to return
+# writes unmixing matrix and independent components to text
 
 import sys
 from numpy import *
@@ -31,6 +30,7 @@ k = int(sys.argv[4])
 c = int(sys.argv[5])
 
 # compute covariance matrix
+print "(ica) computing data covariance"
 data = lines.map(parseVector).cache()
 n = data.count()
 m = len(data.first())
@@ -39,17 +39,20 @@ sub = data.map(lambda x : x - meanVec)
 cov = sub.map(lambda x : outer(x,x)).reduce(lambda x,y : (x + y)) / n
 
 # do eigenvector decomposition
+print "(ica) doing eigendecomposition"
 w, v = eig(cov)
 inds = argsort(w)[::1]
 kEigVecs = v[:,inds[0:k]]
 kEigVals = w[inds[0:k]]
 
 # whiten data
+print "(ica) whitening data"
 whtMat = real(dot(inv(sqrtm(diag(kEigVals))),transpose(kEigVecs)))
-unwhtMat = dot(kEigVecs,sqrtm(diag(kEigVals)))
+unwhtMat = real(dot(kEigVecs,sqrtm(diag(kEigVals))))
 wht = sub.map(lambda x : dot(whtMat,x))
 
 # do multiple independent component extraction
+print "(ica) starting iterative ica"
 B = orth(random.randn(k,c))
 Bold = zeros((k,c))
 iterNum = 0
@@ -63,6 +66,7 @@ while (iterNum < iterMax) & ((1 - minAbsCos) > termTol):
 	print "(ica) starting iteration " + str(iterNum)
 	# update rule for pow3 nonlinearity (todo: add other nonlins)
 	B = wht.map(lambda x : outer(x,dot(x,B) ** 3)).reduce(lambda x,y : x + y) / n - 3 * B
+	print "(ica) orthogonalizing"
 	# orthognalize
 	B = dot(B,real(sqrtm(inv(dot(transpose(B),B)))))
 	# evaluate error
@@ -74,13 +78,14 @@ while (iterNum < iterMax) & ((1 - minAbsCos) > termTol):
 # get unmixing matrix
 W = dot(transpose(B),whtMat)
 
-# get unmixed signals
-sigs = sub.map(lambda x : dot(W,x)).collect()
-
 # save output files
+print("(ica) finished after "+str(iterNum)+"iterations")
 print("(ica) writing output...")
 savetxt("out-W-"+outputFile+".txt",W,fmt='%.8f')
-savetxt("out-sigs-"+outputFile+".txt",sigs,fmt='%.8f')
+for ic in range(0,c):
+	# get unmixed signals
+	sigs = wht.map(lambda x : dot(dot(W[ic,:],unwhtMat),x)).collect()
+	savetxt("out-sigs-"+str(ic)+"-"+outputFile+".txt",sigs,fmt='%.8f')
 
 
 
