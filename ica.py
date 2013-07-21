9cabf11fb4910e140cda148c0226f43a1fcc220c
freeman <the.freeman.lab@gmail.com>
2013-07-21 14:21:40 -0400

    Added logging and saving directly from RDD

--- ica.py
+++ ica.py
@@ -32,36 +32,37 @@ k = int(sys.argv[4])
 c = int(sys.argv[5])
 if not os.path.exists(outputFile):
     os.makedirs(outputFile)
+logging.basicConfig(filename=outputFile+'/'+'stdout.log',level=logging.INFO,format='%(asctime)s %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p')
 
 # load data
-print "(ica) loading data"
+logging.info('(ica) loading data')
 lines = sc.textFile(inputFile)
 
 # compute covariance matrix
 data = lines.map(parseVector).cache()
 n = data.count()
 m = len(data.first())
-print "(ica) mean subtraction"
+logging.info('(ica) mean subtraction')
 meanVec = data.reduce(lambda x,y : x+y) / n
 sub = data.map(lambda x : x - meanVec)
-print "(ica) computing covariance"
+logging.info('(ica) computing covariance')
 cov = sub.map(lambda x : outer(x,x)).reduce(lambda x,y : (x + y)) / n
 
 # do eigenvector decomposition
-print "(ica) doing eigendecomposition"
+logging.info('(ica) doing eigendecomposition')
 w, v = eig(cov)
 inds = argsort(w)[::-1]
 kEigVecs = v[:,inds[0:k]]
 kEigVals = w[inds[0:k]]
 
 # whiten data
-print "(ica) whitening data"
+logging.info('(ica) whitening data')
 whtMat = real(dot(inv(diag(sqrt(kEigVals))),transpose(kEigVecs)))
 unwhtMat = real(dot(kEigVecs,diag(sqrt(kEigVals))))
 wht = sub.map(lambda x : dot(whtMat,x))
 
 # do multiple independent component extraction
-print "(ica) starting iterative ica"
+logging.info('(ica) starting iterative ica')
 B = orth(random.randn(k,c))
 Bold = zeros((k,c))
 iterNum = 0
@@ -72,10 +73,9 @@ errVec = zeros(iterMax)
 
 while (iterNum < iterMax) & ((1 - minAbsCos) > termTol):
 	iterNum += 1
-	print "(ica) starting iteration " + str(iterNum)
+	logging.info('(ica) iteration ' + str(iterNum))
 	# update rule for pow3 nonlinearity (todo: add other nonlins)
 	B = wht.map(lambda x : outer(x,dot(x,B) ** 3)).reduce(lambda x,y : x + y) / n - 3 * B
-	print "(ica) orthogonalizing"
 	# orthognalize
 	B = dot(B,real(sqrtm(inv(dot(transpose(B),B)))))
 	# evaluate error
@@ -88,14 +88,13 @@ while (iterNum < iterMax) & ((1 - minAbsCos) > termTol):
 W = dot(transpose(B),whtMat)
 
 # save output files
-print("(ica) finished after "+str(iterNum)+"iterations")
-print("(ica) writing output...")
-savetxt(outputFile+"/"+"out-W-"+outputFile+".txt",W,fmt='%.8f')
+logging.info('(ica) finished after ' + str(iterNum) + ' iterations')
+logging.info('(ica) writing output...')
+savetxt(outputFile+"/"+"W-comps-"+str(c)+".txt",W,fmt='%.8f')
 
 for ic in range(0,c):
 	# get unmixed signals
-	sigs = sub.map(lambda x : dot(W[ic,:],x)).collect()
-	savetxt(outputFile+"/"+"out-sigs-"+str(ic)+"-"+outputFile+".txt",sigs,fmt='%.8f')
+	sigs = sub.map(lambda x : str(dot(W[ic,:],x))[1:-1]).saveAsTextFile(outputFile+"/"+"sigs-comps-"+str(c))
 
 
 
