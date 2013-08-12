43511d69944b4fd6705fd63d8db8606b70180334
freeman <the.freeman.lab@gmail.com>
2013-08-11 20:28:17 -0400

    Bug fixes

--- ref.py
+++ ref.py
@@ -7,7 +7,7 @@ from scipy.io import *
 from pyspark import SparkContext
 import logging
 
-if len(sys.argv) < 4:
+if len(sys.argv) < 3:
   print >> sys.stderr, \
   "(sta) usage: ref <master> <inputFile_Y> <outputFile>"
   exit(-1)
@@ -20,10 +20,7 @@ def parseVector(line):
 # parse inputs
 sc = SparkContext(sys.argv[1], "ref")
 inputFile_Y = str(sys.argv[2])
-outputFile = str(sys.argv[3]) + "-ref"
-if not os.path.exists(outputFile):
-    os.makedirs(outputFile)
-logging.basicConfig(filename=outputFile+'/'+'stdout.log',level=logging.INFO,format='%(asctime)s %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p')
+outputFile = str(sys.argv[3])
 
 # parse data
 logging.info("(ref) loading data")
@@ -34,5 +31,5 @@ Y = lines_Y.map(parseVector).cache()
 logging.info('(ref) computing reference image')
 ref = Y.map(lambda (k,x) : median(x))
 logging.info('(ref) saving results...')
-savemat(outputFile+"/"+"ref.mat",mdict={'ref':ref.collect()},oned_as='column',do_compression='true')
+savemat(outputFile+"-ref.mat",mdict={'ref':ref.collect()},oned_as='column',do_compression='true')
 #savetxt(outputFile+"/"+"sta-lag-"+nm+".txt",sta.collect(),fmt='%.4f')
