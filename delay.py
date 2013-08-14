6a91a9acdd2b02449cc2219a7bdb5e37af2d6439
freeman <the.freeman.lab@gmail.com>
2013-08-14 17:28:56 -0400

    Build predictor as a matrix

--- delay.py
+++ delay.py
@@ -8,7 +8,7 @@ import logging
 
 if len(sys.argv) < 6:
   print >> sys.stderr, \
-  "(sta) usage: delay <master> <inputFile_A> <inputFile_y> <outputFile> <mxLag>"
+  "(sta) usage: delay <master> <inputFile_X> <inputFile_y> <outputFile> <mxLag>"
   exit(-1)
 
 def parseVector(line):
@@ -20,38 +20,43 @@ def parseVector(line):
 	ts = (ts - mean(ts)) / std(ts)
 	return ts
 
-def getSta(x,y,lags):
-	w = zeros((len(lags),1))
-	for i in arange(len(lags)):
-		w[i] = mean(x * roll(y,int(lags[i])))
-	co = norm(w)
-	wscale = w - min(w)
-	wscale = wscale / max(wscale)
-	ph = dot(lags,wscale/sum(wscale))
-	return [ph,co]
-
 # parse inputs
 sc = SparkContext(sys.argv[1], "delay")
-inputFile_A = str(sys.argv[2])
+inputFile_X = str(sys.argv[2])
 inputFile_y = str(sys.argv[3])
 mxLag = int(sys.argv[5])
 outputFile = str(sys.argv[4]) + "-delay-mxLag-" + str(mxLag)
-lags = arange(2*mxLag) - floor(2*mxLag/2)
+lags = arange(2*mxLag+1) - floor(2*mxLag/2)
 if not os.path.exists(outputFile):
     os.makedirs(outputFile)
 logging.basicConfig(filename=outputFile+'/'+'stdout.log',level=logging.INFO,format='%(asctime)s %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p')
 
 # parse data
 logging.info("(delay) loading data")
-lines_A = sc.textFile(inputFile_A)
+lines_X = sc.textFile(inputFile_A)
 lines_y = sc.textFile(inputFile_y)
 y = array([float(x) for x in lines_y.collect()[0].split(' ')])
 y = (y - mean(y))/std(y)
 A = lines_A.map(parseVector)
 
+# make predictor
+Y = zeros((len(lags),len(y)))
+for i in arange(len(lags)):
+		lag = int(lags[i])
+		tmp = roll(y,lag)
+		if lag < 0:
+			tmp[lag:] = 0
+		if lag > 0:
+			tmp[:lag] = 0
+		Y[i,:] = tmp
+
 # compute sta
 logging.info('(delay) computing delay')
-sta = A.map(lambda x : getSta(x,y,lags))
+sta = A.map(lambda x : dot(Y,x))
+norm = sta.map(lambda x : norm(x)).collect()
+
 logging.info('(delay) saving results...')
-savemat(outputFile+"/"+"ph.mat",mdict={'ph':sta.map(lambda x : x[0]).collect()},oned_as='column',do_compression='true')
-savemat(outputFile+"/"+"co.mat",mdict={'co':sta.map(lambda x : x[1]).collect()},oned_as='column',do_compression='true')
+savemat(outputFile+"/"+"norm.mat",mdict={'norm':norm},oned_as='column',do_compression='true')
+for i in arange(len(lags)):
+	filt = sta.map(lambda x : x[i]).collect()
+	savemat(outputFile+"/"+"filt"+str(i)+".mat",mdict={'filt':filt},oned_as='column',do_compression='true')
