945e211faff70ce3ea0d75a911be31f40a0d1f69
freeman <the.freeman.lab@gmail.com>
2013-08-19 23:20:47 -0400

    New function for reducing dimensionality after triggering

---
+++ lowdim.py
@@ -0,0 +1,62 @@
+import sys
+import os
+from numpy import *
+from scipy.linalg import *
+from scipy.io import * 
+from pyspark import SparkContext
+import logging
+
+if len(sys.argv) < 5:
+  print >> sys.stderr, \
+  "(lowdim) usage: lowdim <master> <inputFile_X> <inputFile_t> <outputFile> <k>"
+  exit(-1)
+
+def parseVector(line):
+	vec = [float(x) for x in line.split(' ')]
+	ts = array(vec[3:]) # get tseries
+	med = median(ts)
+	ts = (ts - med) / (med) # convert to dff
+	return ts
+
+# parse inputs
+sc = SparkContext(sys.argv[1], "lowdim")
+inputFile_X = str(sys.argv[2])
+inputFile_t = str(sys.argv[3])
+outputFile = str(sys.argv[4]) + "-lowdim"
+if not os.path.exists(outputFile):
+    os.makedirs(outputFile)
+logging.basicConfig(filename=outputFile+'/'+'stdout.log',level=logging.INFO,format='%(asctime)s %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p')
+
+# parse data
+logging.info("(lowdim) loading data")
+lines_X = sc.textFile(inputFile_X) # the data
+X = lines_X.map(parseVector).cache()
+t = loadmat(inputFile_t)['trigInds'] # the triggers
+
+# compute triggered responses
+if min(shape(t)) == 1 :
+		resp = X.map(lambda x : mean(x[t[0]==it]))
+else :
+		resp = X.map(lambda x : dot(t,x))
+
+# compute covariance
+n = resp.count()
+meanVec = resp.reduce(lambda x,y : x+y) / resp.count()
+cov = resp.map(lambda x : outer(x-meanVec,x-meanVec)).reduce(lambda x,y : (x + y)) / n
+
+logging.info("(lowdim) doing eigendecomposition")
+w, v = eig(cov)
+w = real(w)
+v = real(v)
+inds = argsort(w)[::-1]
+sortedDim2 = transpose(v[:,inds[0:k]])
+latent = w[inds[0:k]]
+
+logging.info("(lowdim) writing evecs and evals")
+savemat(outputFile+"/"+"evecs.mat",mdict{'evecs':sortedDim2},oned_as='column',do_compression='true')
+savemat(outputFile+"/"+"evals.mat",mdict{'evals':latent},oned_as='column',do_compression='true')
+
+for ik in range(0,k)
+	logging.info("(lowdim) writing scores for pc " + str(ik))
+	out = resp.map(lambda x : inner(x-meanVec,sortedDim2[ik,:]))
+	savemat(outputFile+"/"+"scores-"+str(ik)+".mat",mdict={'scores':out.collect()},oned_as='column',do_compression='true')
