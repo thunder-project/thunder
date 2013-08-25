64c77a302adfe15b8d21dfbe418261043cb4221c
freeman <the.freeman.lab@gmail.com>
2013-08-25 13:35:03 -0400

    Bug fixes

--- lowdim.py
+++ lowdim.py
@@ -14,8 +14,9 @@ if len(sys.argv) < 6:
 def parseVector(line):
 	vec = [float(x) for x in line.split(' ')]
 	ts = array(vec[3:]) # get tseries
-	med = median(ts)
-	ts = (ts - med) / (med) # convert to dff
+	#med = median(ts)
+	#ts = (ts - med) / (med) # convert to dff
+	ts = ts / 5000
 	return ts
 
 # parse inputs
