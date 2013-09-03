b20f960c7c612099906b08375cef8bb228cb5e3a
freeman <the.freeman.lab@gmail.com>
2013-09-03 18:14:23 -0400

    Bug fixes

--- python/ref.py
+++ python/ref.py
@@ -21,7 +21,7 @@ def parseVector(line):
 sc = SparkContext(sys.argv[1], "ref")
 inputFile_X = str(sys.argv[2])
 outputFile = str(sys.argv[3])
-logging.basicConfig(filename=outputFile+'/'+'stdout.log',level=logging.INFO,format='%(asctime)s %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p')
+logging.basicConfig(filename=outputFile+'stdout.log',level=logging.INFO,format='%(asctime)s %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p')
 
 # parse data
 logging.info("(ref) loading data")
@@ -31,13 +31,13 @@ X = lines_X.map(parseVector)
 # get z ordering
 logging.info("(ref) getting z ordering")
 zinds = X.filter(lambda (k,x) : (k[0] == 1000) & (k[1] == 1000)).map(lambda (k,x) : k[2]).collect()
-savemat(outputFile+"/"+"zinds.mat",mdict={'zinds':zinds},oned_as='column',do_compression='true')
+savemat(outputFile+"zinds.mat",mdict={'zinds':zinds},oned_as='column',do_compression='true')
 
 # compute ref
 logging.info('(ref) computing reference image')
 ref = X.map(lambda (k,x) : median(x)).collect()
 logging.info('(ref) saving results...')
-savemat(outputFile+"/"+"ref.mat",mdict={'ref':ref},oned_as='column',do_compression='true')
+savemat(outputFile+"ref.mat",mdict={'ref':ref},oned_as='column',do_compression='true')
 
 # compute projection
 #xproj = X.map(lambda (k,x) : (k[0],median(x))).reduceByKey(lambda x,y : x+y, 3000).collect()
