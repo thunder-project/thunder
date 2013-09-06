2d6c05dd203c2f7db39844d8e60006ff2e396aea
freeman <the.freeman.lab@gmail.com>
2013-09-06 01:14:21 -0400

    Switched to normalized distance metric

--- python/kmeans.py
+++ python/kmeans.py
@@ -17,11 +17,12 @@ def closestPoint(p, centers):
   bestIndex = 0
   closest = float("+inf")
   for i in range(len(centers)):
-    tempDist = sum((p - centers[i]) ** 2)
+    #tempDist = sum((p - centers[i]) ** 2)
+    tempDist = max(1 - p * centers[i],0)
     if tempDist < closest:
       closest = tempDist
       bestIndex = i
-  return bestIndex
+  return (bestIndex, closest)
 
 if len(sys.argv) < 5:
   print >> sys.stderr, \
@@ -39,10 +40,11 @@ logging.basicConfig(filename=outputFile+'/'+'stdout.log',level=logging.INFO,form
 logging.info("(kmeans) loading data")
 lines_X = sc.textFile(inputFile_X)
 X = lines_X.map(parseVector)
-norms = X.map(lambda x : norm(x)).collect()
-X = X.map(lambda x : x / norm(x)).cache()
+X = X.map(lambda x : (x - mean(x)) / std(x)).cache()
 
-kPoints = X.take(k)
+kPoints = array(X.take(k))
+for i in range(len(kPoints)): 
+	kPoints[i] = kPoints[i] - mean(kPoints[i])
 
 convergeDist = 0.001
 tempDist = 1.0
@@ -51,7 +53,7 @@ iteration = 0
 while tempDist > convergeDist:
 	logging.info("(kmeans) starting iteration " + str(iteration))
 	closest = X.map(
-	    lambda p : (closestPoint(p, kPoints), (p, 1)))
+	    lambda p : (closestPoint(p, kPoints)[0], (p, 1)))
 	pointStats = closest.reduceByKey(
 	    lambda (x1, y1), (x2, y2): (x1 + x2, y1 + y2))
 	newPoints = pointStats.map(
@@ -60,12 +62,13 @@ while tempDist > convergeDist:
 	tempDist = sum(sum((kPoints[x] - y) ** 2) for (x, y) in newPoints)
 
 	for (x, y) in newPoints:
-	    kPoints[x] = y
+	    kPoints[x] = y / std(y)
 
 	iteration = iteration + 1
 
 logging.info("(kmeans) saving results")
-labels = X.map( lambda p : closestPoint(p, kPoints)).collect()
+labels = X.map( lambda p : closestPoint(p, kPoints)[0]).collect()
+dists = X.map( lambda p : closestPoint(p, kPoints)[1]).collect()
 savemat(outputFile+"/"+"labels.mat",mdict={'labels':labels},oned_as='column',do_compression='true')
+savemat(outputFile+"/"+"labels.mat",mdict={'dists':dists},oned_as='column',do_compression='true')
 savemat(outputFile+"/"+"centers.mat",mdict={'centers':kPoints},oned_as='column',do_compression='true')
-savemat(outputFile+"/"+"norms.mat",mdict={'norms':norms},oned_as='column',do_compression='true')
