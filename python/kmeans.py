b85e54f102b1757ec4b790462ffff201e018fc69
freeman <the.freeman.lab@gmail.com>
2013-09-06 18:22:59 -0400

    Added options for corr and euclidean distance metrics

--- python/kmeans.py
+++ python/kmeans.py
@@ -7,22 +7,25 @@ from pyspark import SparkContext
 import logging
 
 def parseVector(line):
-	vec = [float(x) for x in line.split(' ')]
-	ts = array(vec[3:]) # get tseries, drop x,y,z coords
-	med = median(ts)
-	ts = (ts - med) / (med) # convert to dff
+	vec = array([float(x) for x in line.split(' ')])
+	#ts = array(vec[3:]) # get tseries, drop x,y,z coords
+	#med = median(ts)
+	#ts = (ts - med) / (med) # convert to dff
+	ts = vec
 	return ts
 
-def closestPoint(p, centers):
-  bestIndex = 0
-  closest = float("+inf")
-  for i in range(len(centers)):
-    #tempDist = sum((p - centers[i]) ** 2)
-    tempDist = max(1 - dot(p,centers[i]),0)
-    if tempDist < closest:
-      closest = tempDist
-      bestIndex = i
-  return (bestIndex, closest)
+def closestPoint(p, centers, dist):
+	bestIndex = 0
+	closest = float("+inf")
+	for i in range(len(centers)):
+		if dist == 'euclidean':
+			tempDist = sum((p - centers[i]) ** 2)
+		if dist == 'corr':
+			tempDist = max(1 - dot(p,centers[i]),0)
+		if tempDist < closest:
+			closest = tempDist
+			bestIndex = i
+	return (bestIndex, closest)
 
 if len(sys.argv) < 5:
   print >> sys.stderr, \
@@ -32,6 +35,7 @@ if len(sys.argv) < 5:
 sc = SparkContext(sys.argv[1], "kmeans")
 inputFile_X = str(sys.argv[2])
 k = int(sys.argv[4])
+dist = str(sys.argv[5])
 outputFile = str(sys.argv[3]) + "-kmeans-" + str(k)
 if not os.path.exists(outputFile):
     os.makedirs(outputFile)
@@ -40,21 +44,27 @@ logging.basicConfig(filename=outputFile+'/'+'stdout.log',level=logging.INFO,form
 logging.info("(kmeans) loading data")
 lines_X = sc.textFile(inputFile_X)
 X = lines_X.map(parseVector)
-X = X.map(lambda x : (x - mean(x)) / std(x)).cache()
+if dist == 'corr':
+	X = X.map(lambda x : (x - mean(x)) / norm(x)).cache()
 
-kPoints = array(X.take(k))
-for i in range(len(kPoints)): 
-	kPoints[i] = kPoints[i] - mean(kPoints[i])
+kPoints = X.take(k)
+if dist == 'corr':
+	kPoints = map(lambda x : x - mean(x), kPoints);
 
-convergeDist = 0.01
+convergeDist = 0.001
 tempDist = 1.0
 iteration = 0
 mxIteration = 100
 
 while (tempDist > convergeDist) & (iteration < mxIteration):
 	logging.info("(kmeans) starting iteration " + str(iteration))
-	closest = X.map(
-	    lambda p : (closestPoint(p, kPoints)[0], (p, 1)))
+	if dist == 'corr':
+		kPointsNorm = map(lambda x : x / norm(x), kPoints)
+		closest = X.map(
+		    lambda p : (closestPoint(p, kPointsNorm, dist)[0], (p, 1)))
+	if dist == 'euclidean':
+		closest = X.map(
+	    lambda p : (closestPoint(p, kPoints, dist)[0], (p, 1)))
 	pointStats = closest.reduceByKey(
 	    lambda (x1, y1), (x2, y2): (x1 + x2, y1 + y2))
 	newPoints = pointStats.map(
@@ -63,13 +73,16 @@ while (tempDist > convergeDist) & (iteration < mxIteration):
 	tempDist = sum(sum((kPoints[x] - y) ** 2) for (x, y) in newPoints)
 
 	for (x, y) in newPoints:
-	    kPoints[x] = y / std(y)
+	    kPoints[x] = y
 
 	iteration = iteration + 1
 
+if dist == 'corr':
+	kPoints = map(lambda x : x / norm(x), kPoints)
+
 logging.info("(kmeans) saving results")
-labels = X.map( lambda p : closestPoint(p, kPoints)[0]).collect()
-dists = X.map( lambda p : closestPoint(p, kPoints)[1]).collect()
+labels = X.map( lambda p : closestPoint(p, kPoints, dist)[0]).collect()
+dists = X.map( lambda p : closestPoint(p, kPoints, dist)[1]).collect()
 savemat(outputFile+"/"+"labels.mat",mdict={'labels':labels},oned_as='column',do_compression='true')
-savemat(outputFile+"/"+"labels.mat",mdict={'dists':dists},oned_as='column',do_compression='true')
+savemat(outputFile+"/"+"dists.mat",mdict={'dists':dists},oned_as='column',do_compression='true')
 savemat(outputFile+"/"+"centers.mat",mdict={'centers':kPoints},oned_as='column',do_compression='true')
