81556c9ec012dce783a21182e2ac012ab8907b26
freeman <the.freeman.lab@gmail.com>
2013-09-08 14:16:55 -0400

    Testing

--- data/streaming_test.py
+++ data/streaming_test.py
@@ -16,8 +16,11 @@ srcPath = str(sys.argv[1])
 targetPath = str(sys.argv[2])
 waitTime = float(sys.argv[3])
 files = glob.glob(srcPath+"*")
+count = 1
 for f in files:
 	cmd = "scp " + f + " " + targetPath 
 	os.system(cmd)
+	print('writing file ' +str(count))
+	count = count + 1
 	time.sleep(waitTime)
 
