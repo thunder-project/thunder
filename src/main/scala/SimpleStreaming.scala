0e9ddeb28fae6fc19513550cff1e6be10e04e7b4
freeman <the.freeman.lab@gmail.com>
2013-09-09 00:41:36 -0400

    Testing

--- src/main/scala/SimpleStreaming.scala
+++ src/main/scala/SimpleStreaming.scala
@@ -64,9 +64,9 @@ object SimpleStreaming {
     val lines = ssc.textFileStream(args(1)) // directory to monitor
     val dataStream = lines.map(parseVector _) // parse data
     val stateStream = dataStream.reduceByKey(_+_).updateStateByKey(updateFunc)
-    stateStream.print()
-    //val sortedStates = stateStream.map(getDiffs _).transform(rdd => rdd.sortByKey(true)).map(x => Vector(x._2(0),x._2(1)))
-    //sortedStates.print()
+    //stateStream.print()
+    val sortedStates = stateStream.map(getDiffs _).transform(rdd => rdd.sortByKey(true)).map(x => Vector(x._2(0),x._2(1)))
+    sortedStates.print()
 
     //sortedStates.foreach(rdd => printVector(rdd,args(2)))
 
