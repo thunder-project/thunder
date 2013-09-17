9749d4fc456eae2e07e5feffed28e925b634416a
freeman <the.freeman.lab@gmail.com>
2013-09-17 17:39:42 -0400

    Made output file argument

--- src/main/scala/hierarchical.scala
+++ src/main/scala/hierarchical.scala
@@ -54,12 +54,12 @@ object hierarchical {
 
   def main(args: Array[String]) {
 
-    if (args.length < 2) {
-      System.err.println("Usage: hierarchical <master> <inputFile> ")
+    if (args.length < 4) {
+      System.err.println("Usage: hierarchical <master> <inputFile> <outputFile> ")
       System.exit(1)
     }
     
-    val sc = new SparkContext("local", "hierarchical", System.getenv("SPARK_HOME"),
+    val sc = new SparkContext(args(0), "hierarchical", System.getenv("SPARK_HOME"),
         List("target/scala-2.9.3/thunder_2.9.3-1.0.jar"))
 
     var data = sc.textFile(args(1)).map(parseVector _).cache()
@@ -96,6 +96,6 @@ object hierarchical {
 //      }
     }
 
-    printVector(clusters,"test.txt")
+    printVector(clusters,args(3))
   }
 }
