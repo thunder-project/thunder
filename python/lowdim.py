3e8e397ca1bb2f12ec53ed06a51e264ff7c382ff
freeman <the.freeman.lab@gmail.com>
2013-09-29 17:43:06 -0400

    Added regression option

--- python/lowdim.py
+++ python/lowdim.py
@@ -44,8 +44,11 @@ X = lines_X.map(parseVector).cache()
 y = loadmat(inputFile_y)['y']
 if mode == 'mean' :
 	resp = X.map(lambda x : dot(y,x))
+if mode == 'standardize' :
+	resp = X.map(lambda x : dot(y,(x-mean(x))/norm(x))))
 if mode == 'regress' : 
-	resp = X.map(lambda x : dot(y,(x-mean(x))/norm(x)))
+	yhat = dot(inv(dot(y,transpose(y))),y)
+	resp = X.map(lambda x : dot(yhat,x))
 
 # compute covariance
 logging.info("(lowdim) getting count")
@@ -69,5 +72,6 @@ savemat(outputFile+"/"+"evals.mat",mdict={'evals':latent},oned_as='column',do_co
 for ik in range(0,k):
 	logging.info("(lowdim) writing scores for pc " + str(ik))
 	#out = X.map(lambda x : float16(inner(dot(y,x) - mean(dot(y,x)),sortedDim2[ik,:])))
-	out = X.map(lambda x : float16(inner(dot(y,(x-mean(x))/norm(x)) - mean(dot(y,(x-mean(x))/norm(x))),sortedDim2[ik,:])))
+	#out = X.map(lambda x : float16(inner(dot(y,(x-mean(x))/norm(x)) - mean(dot(y,(x-mean(x))/norm(x))),sortedDim2[ik,:])))
+	out = resp.map(lambda x : float16(inner(x - mean(x),sortedDim2[ik,:])))
 	savemat(outputFile+"/"+"scores-"+str(ik)+".mat",mdict={'scores':out.collect()},oned_as='column',do_compression='true')
