872662d73d40a0742761983b19ccdc5d0238018e
freeman <the.freeman.lab@gmail.com>
2013-09-29 20:18:02 -0400

    Bug fixes

--- python/lowdim.py
+++ python/lowdim.py
@@ -47,7 +47,7 @@ if mode == 'mean' :
 if mode == 'standardize' :
 	resp = X.map(lambda x : dot(y,(x-mean(x))/norm(x)))
 if mode == 'regress' : 
-	yhat = dot(y,inv(dot(y,transpose(y))))
+	yhat = dot(inv(dot(y,transpose(y))),transpose(y))
 	resp = X.map(lambda x : dot(yhat,x))
 
 # compute covariance
