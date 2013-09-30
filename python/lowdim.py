ff01b1681ba895bcd49c37c156b86c41f53a696a
freeman <the.freeman.lab@gmail.com>
2013-09-30 11:45:36 -0400

    Testing

--- python/lowdim.py
+++ python/lowdim.py
@@ -55,7 +55,7 @@ if mode == 'regress' :
 	resp = X.map(lambda x : dot(yhat,x))
 
 vals = array([0,2,4,6,8,10,12,14,16,20,25,30])
-tuning = resp.map(lambda x : clip(x,0)).map(lambda x : x / max(x)).map(lambda x : dot(x,vals)).collect()
+tuning = resp.map(lambda x : clip(x,0)).map(lambda x : x / sum(x)).map(lambda x : dot(x,vals)).collect()
 savemat(outputFile+"/"+"tuning.mat",mdict={'tuning':tuning},oned_as='column',do_compression='true')
 
 # compute covariance
