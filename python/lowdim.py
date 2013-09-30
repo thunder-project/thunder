a06943bff7092e91448ed0f888ae7d42964a9f64
freeman <the.freeman.lab@gmail.com>
2013-09-30 11:31:44 -0400

    Testing

--- python/lowdim.py
+++ python/lowdim.py
@@ -52,7 +52,7 @@ if mode == 'regress' :
 
 vals = array([0,2,4,6,8,10,12,14,16,20,25,30])
 tuning = resp.map(lambda x : dot(x,vals)).collect()
-savemat(outputFile+"/"+"tuning-"+str(ik)+".mat",mdict={'tuning':tuning},oned_as='column',do_compression='true')
+savemat(outputFile+"/"+"tuning.mat",mdict={'tuning':tuning},oned_as='column',do_compression='true')
 
 # compute covariance
 logging.info("(lowdim) getting count")
