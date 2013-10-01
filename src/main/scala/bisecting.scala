c57f536cf6cc82271157c20b022fc08bb73ceb76
freeman <the.freeman.lab@gmail.com>
2013-10-01 11:44:29 -0400

    Restored image writing

--- src/main/scala/bisecting.scala
+++ src/main/scala/bisecting.scala
@@ -74,6 +74,7 @@ object bisecting {
     val img = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB)
     val raster = img.getRaster()
     (X,Y,RGB).zipped.foreach{case(x,y,rgb) => println(x); println(y); raster.setPixel(x-1, y-1, Array(rgb,rgb,rgb))}
+    ImageIO.write(img, "png", new File(fileName))
   }
 
   def closestPoint(p: Vector, centers: Array[Vector]): Int = {
