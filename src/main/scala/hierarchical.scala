c63305ec731797b0849a0fc6a1a3514636ddc0ee
freeman <the.freeman.lab@gmail.com>
2013-10-13 04:06:35 -0400

    Testing

--- src/main/scala/hierarchical.scala
+++ src/main/scala/hierarchical.scala
@@ -79,7 +79,7 @@ object hierarchical {
     System.setProperty("spark.serializer", "spark.KryoSerializer")
     //System.setProperty("spark.default.parallelism", "50")
     val sc = new SparkContext(args(0), "hierarchical", System.getenv("SPARK_HOME"),
-      List("target/scala-2.9.3/thunder_2.9.3-1.0.jar"))
+      List("target/scala-2.9.3/thunder_2.9.3-1.0.jar","project/spray-json_2.9.3-1.2.5.jar"))
     //sc.setCheckpointDir(System.getenv("CHECKPOINT"))
 
     var data = sc.textFile(args(1)).map(parseVector _).cache()
