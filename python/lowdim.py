45c527796426b3eacf174a96ab828b73c50c66e6
freeman <the.freeman.lab@gmail.com>
2013-10-14 01:32:26 -0400

    Bug fixes

--- python/lowdim.py
+++ python/lowdim.py
@@ -35,7 +35,7 @@ def clip(vec,val):
 def threshMap(x,y,eigs,rng1,rng2):
 	vals = inner(dot(y,x) - mean(dot(y,x)),eigs)
 	r = sqrt(vals[0]**2 + vals[1]**2)
-	t = arctan2(vals[0], vals[1])
+	t = arctan2(vals[1], vals[0])
 	out = zeros(shape(x))
 	if (t > rng1) & (t < rng2):
 		out = x * r
