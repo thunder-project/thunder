bd1b87bf9f96503a525121a1a2da58e05dcb4a28
freeman <the.freeman.lab@gmail.com>
2013-10-21 19:56:32 -0400

    Testing

--- python/localcorr.py
+++ python/localcorr.py
@@ -68,18 +68,20 @@ neighbors = X.flatMap(lambda (k,v) : mapToNeighborhood(k,v,sz,mxX,mxY))
 # reduceByKey to get the average time series for each neighborhood
 means = neighbors.reduceByKey(lambda x,y : x + y).map(lambda (k,v) : (k, v / ((2*sz+1)**2)))
 
+print(means.first())
+
 # join with the original time series data to compute correlations
-result = X.join(means)
+# result = X.join(means)
 
-print(result.first())
+# print(result.first())
 
-# get correlations
-corr = result.map(lambda (k,v) : mean(v[0]))
+# # get correlations
+# corr = result.map(lambda (k,v) : mean(v[0]))
 
-# return keys because we'll need to sort on them post-hoc
-# TODO: use sortByKey once implemented in pyspark
-keys = result.map(lambda (k,v) : k)
+# # return keys because we'll need to sort on them post-hoc
+# # TODO: use sortByKey once implemented in pyspark
+# keys = result.map(lambda (k,v) : k)
 
-savemat(outputFile+"/"+"corr.mat",mdict={'corr':result.collect()},oned_as='column',do_compression='true')
+# savemat(outputFile+"/"+"corr.mat",mdict={'corr':result.collect()},oned_as='column',do_compression='true')
 
-savemat(outputFile+"/"+"keys.mat",mdict={'keys':keys.collect()},oned_as='column',do_compression='true')
+# savemat(outputFile+"/"+"keys.mat",mdict={'keys':keys.collect()},oned_as='column',do_compression='true')
