e8368a6d655c7bd6ebef9c4f0062f4e17234248e
freeman <the.freeman.lab@gmail.com>
2013-10-21 19:53:02 -0400

    Testing

--- python/localcorr.py
+++ python/localcorr.py
@@ -71,6 +71,8 @@ means = neighbors.reduceByKey(lambda x,y : x + y).map(lambda (k,v) : (k, v / ((2
 # join with the original time series data to compute correlations
 result = X.join(means)
 
+print(result.first())
+
 # get correlations
 corr = result.map(lambda (k,v) : mean(v[0]))
 
