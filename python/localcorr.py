10fbfda4b57e3bb66551dbebdbd2d355ef8840e5
freeman <the.freeman.lab@gmail.com>
2013-10-21 02:09:53 -0400

    First major commit

--- python/localcorr.py
+++ python/localcorr.py
@@ -14,7 +14,7 @@ import logging
 
 if len(sys.argv) < 6:
   print >> sys.stderr, \
-  "(localcorr) usage: localcorr <master> <inputFile_X> <outputFile> <neighborhood>"
+  "(localcorr) usage: localcorr <master> <inputFile_X> <outputFile> <sz>"
   exit(-1)
 
 def parseVector(line):
@@ -24,11 +24,22 @@ def parseVector(line):
 	ts = (ts - meanVal) / (meanVal + 0.1) # convert to dff
 	return ((int(vec[0]),int(vec[1]),int(vec[2])),ts) # (x,y,z),(tseries) pair 
 
+def mapToNeighborhood((ind,ts),sz):
+	rngX = range(-sz,sz+1,1)
+	rngY = range(-sz,sz+1,1)
+	out = list((ind,ts))
+	for x in rngX :
+		for y in rngY :
+			if ~(x ==0 & y == 0) :
+				newind = (ind[0] + x, ind[1] + y, ind[2])
+				out.append((newind,ts))
+	return out
+
 # parse inputs
 sc = SparkContext(sys.argv[1], "localcorr")
 inputFile_X = str(sys.argv[2])
 outputFile = str(sys.argv[3]) + "-localcorr"
-neighborhood = int(sys.argv[4])
+sz = int(sys.argv[4])
 if not os.path.exists(outputFile):
     os.makedirs(outputFile)
 logging.basicConfig(filename=outputFile+'/'+'stdout.log',level=logging.INFO,format='%(asctime)s %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p')
@@ -38,12 +49,11 @@ logging.info("(lowdim) loading data")
 lines_X = sc.textFile(inputFile_X) # the data
 X = lines_X.map(parseVector).cache()
 
-# flatmap each time series to a bunch of key value pairs where the key is now a neighborhood identifier, the first value is the key from before, and the value is the time series
+# flatmap each time series to key value pairs where the key is now a neighborhood identifier and the value is the time series
+neighbors = X.flatMap(lambda (k,v) : mapToNeighborhood((k,v),sz))
 
-# reduceByKey to get an RDD where the keys are now the indices and the value is the average time series
+# reduceByKey to get the average time series
+means = neighbors.reduceByKey(lambda x,y : x + y).map(lambda x : x / sz)
 
-# join with the original data set
-
-# correlate the two values for each key
-
-# return the correlation coefficient
+# join with the original data set and compute correlations
+result = X.join(means).map(lambda (k,v) : corrcoef(v[0],v[1]))
