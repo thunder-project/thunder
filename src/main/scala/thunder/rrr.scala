4c9ce6a759ca9e1a209810ea19c1f60dd3f000f8
freeman <the.freeman.lab@gmail.com>
2013-10-26 01:42:06 -0400

    Testing

--- src/main/scala/thunder/rrr.scala
+++ src/main/scala/thunder/rrr.scala
@@ -32,10 +32,10 @@ object rrr {
   def parseVector(line: String): ((Array[Int]), DoubleMatrix1D) = {
     var vec = line.split(' ').drop(3).map(_.toDouble)
     val inds = line.split(' ').take(3).map(_.toDouble.toInt) // xyz coords
-    val n = vec.length
-    val sortVals = vec.sorted
-    val mean = sortVals((n/4).toInt)
-    //val mean = vec.sum / vec.length
+    //val n = vec.length
+    //val sortVals = vec.sorted
+    //val mean = sortVals((n/4).toInt)
+    val mean = vec.sum / vec.length
     vec = vec.map(x => (x - mean)/(mean + 0.1)) // time series
     return (inds,factory1D.make(vec))
   }
