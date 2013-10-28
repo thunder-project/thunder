baea3899d5ce69ae74151ae65c2a34b209dba121
freeman <the.freeman.lab@gmail.com>
2013-10-28 02:30:16 -0400

    Restored mean subtraction

--- python/lowdim.py
+++ python/lowdim.py
@@ -168,9 +168,9 @@ if outputMode == 'maps':
 
 if outputMode == 'tuning':
 	xvals = arange(0,2*pi,2*pi/12)
-	r = resp.map(lambda x : float16(sqrt(sum(inner(x,sortedDim2) ** 2))))
+	r = resp.map(lambda x : float16(sqrt(sum(inner(x-mean(x),sortedDim2) ** 2))))
 	savemat(outputFile+"/"+"r.mat",mdict={'r':r.collect()},oned_as='column',do_compression='true')
-	p = resp.map(lambda x : float16(getTuningParams(xvals,dot(inner(x,sortedDim2),sortedDim2))))
+	p = resp.map(lambda x : float16(getTuningParams(xvals,dot(inner(x-mean(x),sortedDim2),sortedDim2))))
 	savemat(outputFile+"/"+"tuning-param.mat",mdict={'p':p.collect()},oned_as='column',do_compression='true')
 
 if outputMode == 'pie':
