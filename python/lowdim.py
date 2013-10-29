bd1f3c86bfc22fbfd02be61fb9a81e1a5081d409
freeman <the.freeman.lab@gmail.com>
2013-10-29 18:44:16 -0400

    Testing map on X to avoid stall out

--- python/lowdim.py
+++ python/lowdim.py
@@ -162,8 +162,8 @@ if outputMode == 'traj':
 if outputMode == 'maps':
 	for ik in range(0,k):
 		logging.info("(lowdim) writing scores for pc " + str(ik))
-		#out = X.map(lambda x : float16(inner(dot(y,x) - mean(dot(y,x)),sortedDim2[ik,:])))
-		out = resp.map(lambda x : float16(inner(x - mean(x),sortedDim2[ik,:])))
+		out = X.map(lambda x : float16(inner(dot(y,x) - mean(dot(y,x)),sortedDim2[ik,:])))
+		#out = resp.map(lambda x : float16(inner(x - mean(x),sortedDim2[ik,:])))
 		savemat(outputFile+"/"+"scores-"+str(ik)+".mat",mdict={'scores':out.collect()},oned_as='column',do_compression='true')
 
 if outputMode == 'tuning':
