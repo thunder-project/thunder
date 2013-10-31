1032c52bfc88c28b7cee0af8746750367a19f606
freeman <the.freeman.lab@gmail.com>
2013-10-31 17:11:11 -0400

    Bug fix

--- python/lowdim.py
+++ python/lowdim.py
@@ -148,7 +148,7 @@ if analMode == 'regress2' :
 	resp = X.map(lambda x : getRegression(x,yhat))
 	p1 = resp.map(lambda r : norm(r[0:20])).collect()
 	p2 = resp.map(lambda r : norm(r[20:40])).collect()
-	p3 = resp.map(lambda x : getTiming(r[20:40])).collect()
+	p3 = resp.map(lambda r : getTiming(r[20:40])).collect()
 	savemat(outputFile+"/"+"p1.mat",mdict={'p1':p1},oned_as='column',do_compression='true')
 	savemat(outputFile+"/"+"p2.mat",mdict={'p2':p2},oned_as='column',do_compression='true')
 	savemat(outputFile+"/"+"p3.mat",mdict={'p3':p3},oned_as='column',do_compression='true')
