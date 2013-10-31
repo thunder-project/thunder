7599c280d93de7ecac62d2f8c8d912dedc15b398
freeman <the.freeman.lab@gmail.com>
2013-10-31 16:05:36 -0400

    Testing

--- python/lowdim.py
+++ python/lowdim.py
@@ -141,8 +141,11 @@ if analMode == 'regress' :
 if analMode == 'regress2' : 
 	yhat = dot(inv(dot(y,transpose(y))),y)
 	resp = X.map(lambda x : getRegression(x,yhat))
-	p1 = resp.map(lambda r : norm(r[0,20]))
-	p2 = resp.map(lambda r : norm(r[20,40]))
+	p1 = resp.map(lambda r : norm(r[0,20])).collect()
+	p2 = resp.map(lambda r : norm(r[20,40])).collect()
+	savemat(outputFile+"/"+"p1.mat",mdict={'p1':p1},oned_as='column',do_compression='true')
+	savemat(outputFile+"/"+"p2.mat",mdict={'p2':p2},oned_as='column',do_compression='true')
+
 
 # compute covariance
 logging.info("(lowdim) getting count")
