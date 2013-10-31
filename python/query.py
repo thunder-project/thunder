22981272321301cc861480db023e0e38ee8d372d
freeman <the.freeman.lab@gmail.com>
2013-10-31 00:55:30 -0400

    Changed constant

--- python/query.py
+++ python/query.py
@@ -20,7 +20,7 @@ if len(sys.argv) < 4:
 def parseVector(line):
 	vec = [float(x) for x in line.split(' ')]
 	ts = array(vec[3:]) # get tseries
-	k = int(vec[0]) + int((vec[1] - 1)*1248) # constant is the max value along first dimension
+	k = int(vec[0]) + int((vec[1] - 1)*2048) # constant is the max value along first dimension
 	meanVal = mean(ts)
 	ts = (ts - meanVal) / (meanVal + 0.1) # convert to dff
 	return (k,ts)
