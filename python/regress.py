8044406b20508499c81c337ae155667d32b126f4
freeman <the.freeman.lab@gmail.com>
2013-10-31 23:38:15 -0400

    Major refactoring of regression function

--- python/regress.py
+++ python/regress.py
@@ -1,10 +1,10 @@
-# regress <master> <inputFile_Y> <inputFile_x1> <inputFile_x2> <outputFile> <mxLag>
+# regress <master> <inputFile_Y> <inputFile_X> <outputFile> <analMode> <outputMode>
 # 
 # time series regression on a data matrix
 # each row is (x,y,z,timeseries)
 # inputs are signals to regress against
-#
-# TODO: add options for number of regressors and interactions
+# can process results either by doing dimensionality reduction
+# or by fitting a parametric model
 #
 
 import sys
@@ -15,61 +15,118 @@ from scipy.io import *
 from pyspark import SparkContext
 import logging
 
-if len(sys.argv) < 7:
+argsIn = sys.argv[1:]
+
+if len(argsIn) < 7:
   print >> sys.stderr, \
-  "(regress) usage: regress <master> <inputFile_Y> <inputFile_x1> <inputFile_x2> <outputFile> <mxLag>"
+  "(regress) usage: regress <master> <inputFile_Y> <inputFile_X> <outputFile> <analMode> <outputMode>"
   exit(-1)
 
-def parseVector(line):
+def parseVector(line,mode="raw",xyz=0,inds=None):
 	vec = [float(x) for x in line.split(' ')]
 	ts = array(vec[3:]) # get tseries
-	med = median(ts)
-	ts = (ts - med) / (med + 0.1) # convert to dff
-	return ts
+	if inds is not None :
+		ts = ts[inds[0]:inds[1]]
+	if mode == "dff" :
+		meanVal = mean(ts)
+		ts = (ts - meanVal) / (meanVal + 0.1)
+	if xyz == 1 :
+		return ((int(vec[0]),int(vec[1]),int(vec[2])),ts)
+	else :
+		return ts
 
-def getRegStats(y,pinvX):
-	y = y[hstack((arange(0,93),arange(130,259)))]
-	betaest = dot(transpose(y),pinvX)
-	predic = dot(betaest,X)
-	SSE = sum((predic - y) ** 2)
-	SST = sum((y - mean(y)) ** 2)
-	r2 = 1 - SSE/SST
-	return hstack((betaest,r2))
+def getRegression(y,model) :
+	if model.regressMode == 'mean' :
+		return dot(model.X,y)
+	if model.regressMode == 'linear' :
+		return dot(model.Xhat,y)[1:]
+	if model.regressMode == 'bilinear' :
+		return dot(model.Xhat,y)
 
 # parse inputs
-sc = SparkContext(sys.argv[1], "regress")
-inputFile_Y = str(sys.argv[2])
-inputFile_x1 = str(sys.argv[3])
-inputFile_x2 = str(sys.argv[4])
-mxLag = int(sys.argv[6])
-outputFile = str(sys.argv[5]) + "-regress-mxLag-" + str(mxLag)
-lags = arange(2*mxLag) - floor(2*mxLag/2)
-if not os.path.exists(outputFile):
-    os.makedirs(outputFile)
+sc = SparkContext(argsIn[0], "regress")
+inputFile_Y = str(argsIn[1])
+inputFile_X = str(argsIn[2])
+outputFile = str(argsIn[3]) + "-regress"
+regressMode = str(argsIn[4])
+outputMode = str(argsIn[5])
+
+if not os.path.exists(outputFile) :
+	os.makedirs(outputFile)
 logging.basicConfig(filename=outputFile+'/'+'stdout.log',level=logging.INFO,format='%(asctime)s %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p')
 
 # parse data
 logging.info("(regress) loading data")
 lines_Y = sc.textFile(inputFile_Y)
-lines_x1 = sc.textFile(inputFile_x1)
-lines_x2 = sc.textFile(inputFile_x2)
-x1 = array([float(x) for x in lines_x1.collect()[0].split(' ')])
-x2 = array([float(x) for x in lines_x2.collect()[0].split(' ')])
-Y = lines_Y.map(parseVector).cache()
-n = len(x1)
 
-# compute sta
-for lag in lags:
-	logging.info('(regress) doing regression with time lag ' + str(lag))
-	X = vstack((ones((n,)),roll(x1,int(lag)),roll(x2,int(lag)),roll(x1*x2,int(lag))))
-	X = X[:,hstack((arange(0,93),arange(130,259)))]
-	Xpinv = pinv(X)
-	betas = Y.map(lambda y : getRegStats(y,Xpinv))
-	logging.info('(regress) saving results...')
-	nm = str(int(lag))
-	if (lag < 0):
-		nm = "n" + nm[1:]
-	savemat(outputFile+"/"+"b1-lag-"+nm+".mat",mdict={'b1':betas.map(lambda x : x[1]).collect()},oned_as='column',do_compression='true')
-	savemat(outputFile+"/"+"b2-lag-"+nm+".mat",mdict={'b2':betas.map(lambda x : x[2]).collect()},oned_as='column',do_compression='true')
-	savemat(outputFile+"/"+"b12-lag-"+nm+".mat",mdict={'b12':betas.map(lambda x : x[3]).collect()},oned_as='column',do_compression='true')
-	savemat(outputFile+"/"+"r2-lag-"+nm+".mat",mdict={'r2':betas.map(lambda x : x[4]).collect()},oned_as='column',do_compression='true')
+# parse model
+class model : pass
+model.regressMode = regressMode
+if regressMode == 'mean' :
+	X = loadmat(inputFile_X + "_X")['X']
+	X = X.astype(float)
+	model.X = X
+if regressMode == 'linear' :
+	X = loadmat(inputFile_X + "_X")['X']
+	X = X.astype(float)
+	Xhat = dot(inv(dot(X,transpose(X))),X)
+	model.X = X
+	model.Xhat = Xhat
+if regressMode == 'bilinear' :
+	X1 = loadmat(inputFile_X + "_X1")['X1']
+	X2 = loadmat(inputFile_X + "_X2")['X2']
+	X1hat = dot(inv(dot(X1,transpose(X1))),X1)
+	X2hat = dot(inv(dot(X2,transpose(X2))),X2)
+	model.X1 = X1
+	model.X2 = X2
+	model.X1hat = X1hat
+	model.X2hat = X2hat
+if outputMode == 'tuning' :
+	s = loadmat(inputFile_X + "_s")['s']
+	model.s = s
+
+# compute parameter estimates
+B = Y.map(lambda y : getRegression(y,model))
+
+# process outputs using pca
+if outputMode == 'pca'
+	n = B.count()
+	cov = B.map(lambda b : outer(b-mean(b),b-mean(b))).reduce(lambda x,y : (x + y)) / n
+	w, v = eig(cov)
+	w = real(w)
+	v = real(v)
+	inds = argsort(w)[::-1]
+	comps = transpose(v[:,inds[0:k]])
+	savemat(outputFile+"/"+"comps.mat",mdict={'comps':comps},oned_as='column',do_compression='true')
+	latent = w[inds[0:k]]
+	for ik in range(0,k) :
+		scores = Y.map(lambda y : float16(inner(getRegression(y,model) - mean(getRegression(y,model)),comps[ik,:])))
+		savemat(outputFile+"/"+"scores-"+str(ik)+".mat",mdict={'scores':scores.collect()},oned_as='column',do_compression='true')
+	traj = Y.map(lambda x : outer(x,inner(getRegression(y,model) - mean(getRegression(y,model)),comps))).reduce(lambda x,y : x + y) / n
+	savemat(outputFile+"/"+"traj.mat",mdict={'traj':traj},oned_as='column',do_compression='true')
+
+# process output with a parametric tuning curve
+if outputMode == 'tuning' :
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
