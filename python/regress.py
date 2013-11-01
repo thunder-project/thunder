3ca210178dc6ac5a7de73a0bcca8f5c5598f3bd8
freeman <the.freeman.lab@gmail.com>
2013-11-01 13:24:22 -0400

    Testing

--- python/regress.py
+++ python/regress.py
@@ -66,8 +66,8 @@ def getRegression(y,model) :
 def getTuning(y,model) :
 	if model.tuningMode == 'circular' :
 		z = norm(y)
-		if min(y) < 0 :
-			y = y - min(y)
+		y = y - min(y)
+		#	y = y - min(y)
 		y = y/sum(y)
 		r = inner(y,exp(1j*model.s))
 		mu = angle(r)
