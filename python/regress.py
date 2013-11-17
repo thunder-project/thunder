98da64e2391662bed041f41c41d682d160024b55
freeman <the.freeman.lab@gmail.com>
2013-11-17 17:58:36 -0500

    Bug fix

--- python/regress.py
+++ python/regress.py
@@ -212,7 +212,7 @@ if outputMode == 'tuning' :
 	savemat(outputFile+"/"+"p.mat",mdict={'p':p},oned_as='column',do_compression='true')
 	# get average tuning for groups of pixels
 	vals = linspace(min(model.s),max(model.s),len(model.s))
-	tuningCurves = zeros((len(model.s)-1),len(model.s))
+	tuningCurves = zeros((len(model.s)-1,len(model.s)))
 	for iv in range(0,len(model.s)-1) :
 		subset = B.filter(lambda b : inRange(getTuning(b[0],model),vals[iv],vals[iv+1]))
 		tuningCurves[iv,:] = subset.map(lambda b : b[0]).reduce(lambda x,y : x + y) / subset.count()
