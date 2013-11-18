030f293b4c24436e451a60fca346da8a9849d2ee
freeman <the.freeman.lab@gmail.com>
2013-11-17 22:57:09 -0500

    Testing

--- python/regress.py
+++ python/regress.py
@@ -66,7 +66,7 @@ def getRegression(y,model) :
 
 	if model.regressMode == 'linear-shuffle' :
 		#b = dot(model.Xhat,y)
-		b = lstsq(transpose(model.X),transpose(y))
+		b = lstsq(transpose(model.X),y)
 		predic = dot(b,model.X)
 		sse = sum((predic-y) ** 2)
 		sst = sum((y-mean(y)) ** 2)
