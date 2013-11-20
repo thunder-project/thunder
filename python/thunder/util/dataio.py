cce11e1be55baa583eb9c9a89ef481a7da0c47f6
freeman <the.freeman.lab@gmail.com>
2013-11-20 00:34:36 -0500

    Testing

--- python/thunder/util/dataio.py
+++ python/thunder/util/dataio.py
@@ -25,7 +25,5 @@ def saveout(data, outputDir, outputFile, outputFormat) :
 
 	if outputFormat == "matlab" :
 		dtype = type(data) 
-		if (dtype == pyspark.rdd.RDD) | (dtype == pyspark.rdd.PipelinedRDD) :
-			savemat(outputDir+"/"+outputFile+".mat",mdict={outputFile : data.map(float16).collect()},oned_as='column',do_compression='true')
-		else :
+		#
 			savemat(outputDir+"/"+outputFile+".mat",mdict={outputFile : data},oned_as='column',do_compression='true')
