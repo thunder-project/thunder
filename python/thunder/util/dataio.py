ca33e1df1d98fbe17f5bfc7fc2ed4686d1bf3a1e
freeman <the.freeman.lab@gmail.com>
2013-11-20 00:25:42 -0500

    Testing

--- python/thunder/util/dataio.py
+++ python/thunder/util/dataio.py
@@ -8,17 +8,7 @@ def parseVector(line, filter="raw", inds=None):
 
 	vec = [float(x) for x in line.split(' ')]
 	ts = array(vec[3:]) # get tseries
-	if filter == "dff" : # convert to dff
-		meanVal = mean(ts)
-		ts = (ts - meanVal) / (meanVal + 0.1)
-	if inds is not None :
-		if inds == "xyz" :
-			return ((int(vec[0]),int(vec[1]),int(vec[2])),ts)
-		if inds == "linear" :
-			k = int(vec[0]) + int((vec[1] - 1)*1650)
-			return (k,ts)
-	else :
-		return ts
+	return ts
 	
 def saveout(data, outputDir, outputFile, outputFormat) :
 
