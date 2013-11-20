b134b1e8cbf791c6a586b2c8f0d3b429da733572
freeman <the.freeman.lab@gmail.com>
2013-11-20 01:53:00 -0500

    Eliminated conversion to float from dataio

--- python/thunder/util/dataio.py
+++ python/thunder/util/dataio.py
@@ -30,6 +30,6 @@ def saveout(data, outputDir, outputFile, outputFormat) :
 	if outputFormat == "matlab" :
 		dtype = type(data) 
 		if (dtype == pyspark.rdd.RDD) | (dtype == pyspark.rdd.PipelinedRDD) :
-			savemat(outputDir+"/"+outputFile+".mat",mdict={outputFile : data.map(float16).collect()},oned_as='column',do_compression='true')
+			savemat(outputDir+"/"+outputFile+".mat",mdict={outputFile : data.collect()},oned_as='column',do_compression='true')
 		else :
 			savemat(outputDir+"/"+outputFile+".mat",mdict={outputFile : data},oned_as='column',do_compression='true')
