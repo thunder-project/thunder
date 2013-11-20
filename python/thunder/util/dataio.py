6bad223f0d91fa79a2d6db21f960e2000edf0b19
freeman <the.freeman.lab@gmail.com>
2013-11-20 00:37:32 -0500

    Testing

--- python/thunder/util/dataio.py
+++ python/thunder/util/dataio.py
@@ -2,8 +2,11 @@
 
 from scipy.io import * 
 from numpy import *
-#import pyspark
+import pyspark
 
+
+def parse(rdd) :
+	return rdd.map(lambda x : parseVector(x))
 
 def parseVector(line, filter="raw", inds=None):
 
@@ -25,5 +28,7 @@ def saveout(data, outputDir, outputFile, outputFormat) :
 
 	if outputFormat == "matlab" :
 		dtype = type(data) 
-		#
-		savemat(outputDir+"/"+outputFile+".mat",mdict={outputFile : data.collect()},oned_as='column',do_compression='true')
+		if (dtype == pyspark.rdd.RDD) | (dtype == pyspark.rdd.PipelinedRDD) :
+			savemat(outputDir+"/"+outputFile+".mat",mdict={outputFile : data.map(float16).collect()},oned_as='column',do_compression='true')
+		else :
+			savemat(outputDir+"/"+outputFile+".mat",mdict={outputFile : data},oned_as='column',do_compression='true')
