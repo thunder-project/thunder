89cc048d87478cd9ff0554435fbb3dd7e7918e79
freeman <the.freeman.lab@gmail.com>
2013-11-20 02:06:05 -0500

    Remove collect from dataio

--- python/thunder/summary/localcorr.py
+++ python/thunder/summary/localcorr.py
@@ -63,8 +63,12 @@ result = data.join(means)
 
 # get correlations and keys
 # TODO: use sortByKey to avoid returning keys once implemented in pyspark
-corr = result.map(lambda (k,v) : (k,float16(corrcoef(v[0],v[1])[0,1])))
+corr = result.map(lambda (k,v) : (k,corrcoef(v[0],v[1])[0,1])).cache()
 
-print(corr.first())
+saveout(corr.map(lambda (k,v) : k[0])),outputFile,"x","matlab")
+saveout(corr.map(lambda (k,v) : k[1])),outputFile,"x","matlab")
+saveout(corr.map(lambda (k,v) : float16(v))),outputFile,"x","matlab")
+
+#print(corr.first())
 #savemat(outputFile+"/"+"corr.mat",mdict={"corr" : corr.collect()},oned_as='column',do_compression='true')
 #saveout(corr,outputFile,"corr","matlab")
--- python/thunder/util/dataio.py
+++ python/thunder/util/dataio.py
@@ -28,8 +28,4 @@ def parse(data, filter="raw", inds=None) :
 def saveout(data, outputDir, outputFile, outputFormat) :
 
 	if outputFormat == "matlab" :
-		dtype = type(data) 
-		if (dtype == pyspark.rdd.RDD) | (dtype == pyspark.rdd.PipelinedRDD) :
-			savemat(outputDir+"/"+outputFile+".mat",mdict={outputFile : data.collect()},oned_as='column',do_compression='true')
-		else :
-			savemat(outputDir+"/"+outputFile+".mat",mdict={outputFile : data},oned_as='column',do_compression='true')
+		savemat(outputDir+"/"+outputFile+".mat",mdict={outputFile : data},oned_as='column',do_compression='true')
