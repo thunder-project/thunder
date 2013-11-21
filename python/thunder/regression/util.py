25f2469e4d0f832aa5f3f4ae6b0ac75800b13739
freeman <the.freeman.lab@gmail.com>
2013-11-21 14:10:04 -0500

    Bug fix

--- python/thunder/regression/util.py
+++ python/thunder/regression/util.py
@@ -162,16 +162,14 @@ def tuningCurves(data,model) :
 		else:
 			return False
 
-	print(amin(model.s))
-	print(amax(model.s))
 	vals = linspace(amin(model.s),amax(model.s),6)
 	means = zeros((len(vals)-1,len(model.s)))
 	sds = zeros((len(vals)-1,len(model.s)))
 	for iv in range(0,len(vals)-1) :
 		subset = data.filter(lambda b : (b[1] > 0.005) & inRange(tuningGet(b[0],model)[0],vals[iv],vals[iv+1]))
 		n = subset.count()
-		means[iv,:] = data.map(lambda b : b[0]).reduce(lambda x,y : x + y) / n
-		sds[iv,:] = data.map(lambda b : (b[0] - means[iv,:])**2).reduce(lambda x,y : x + y) / (n - 1)
+		means[iv,:] = subset.map(lambda b : b[0]).reduce(lambda x,y : x + y) / n
+		sds[iv,:] = subset.map(lambda b : (b[0] - means[iv,:])**2).reduce(lambda x,y : x + y) / (n - 1)
 	
 	return means, sds
 
