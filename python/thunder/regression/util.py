6485d6eb48c93a9b1374d860c79c160ed73282c2
freeman <the.freeman.lab@gmail.com>
2013-11-21 15:14:23 -0500

    Testing

--- python/thunder/regression/util.py
+++ python/thunder/regression/util.py
@@ -170,8 +170,8 @@ def tuningCurves(data,model) :
 		print(subset.first())
 		n = subset.count()
 		print(n)
-		print(subset.map(lambda b : b[0]).reduce(lambda x,y : x + y) / n)
-		means[:,iv] = array(subset.map(lambda b : b[0]).reduce(lambda x,y : x + y) / n)
+		print(array(subset.map(lambda b : b[0]).reduce(lambda x,y : x + y) / n))
+		means[iv,:] = array(subset.map(lambda b : b[0]).reduce(lambda x,y : x + y) / n)
 		sds[iv,:] = subset.map(lambda b : (b[0] - means[iv,:])**2).reduce(lambda x,y : x + y) / (n - 1)
 	
 	return means, sds
