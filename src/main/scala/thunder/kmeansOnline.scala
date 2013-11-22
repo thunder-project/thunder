9fb5ecc57c52cbe7a0e0964709ebc26cda3b2a48
freeman <the.freeman.lab@gmail.com>
2013-11-22 01:30:23 -0500

    Offset indices by 1

--- src/main/scala/thunder/kmeansOnline.scala
+++ src/main/scala/thunder/kmeansOnline.scala
@@ -14,7 +14,7 @@ object kmeansOnline {
   def parseVector(line: String, t: Int): (Int, Vector) = {
     val nums = line.split(' ') // split line into numbers: (0) key (1) ca (2) id
     val k = nums(0).toInt // get index as key
-    val id = nums(2).toInt
+    val id = nums(2).toInt - 1
     val vals, counts = Array.fill[Double](t)(0)
     vals(id) = nums(1).toDouble
     counts(id) += 1
