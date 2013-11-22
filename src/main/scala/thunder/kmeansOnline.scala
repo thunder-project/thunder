285ecccc3aa2e3e78e5504839d3f4f7149827447
freeman <the.freeman.lab@gmail.com>
2013-11-22 02:37:53 -0500

    Testing

--- src/main/scala/thunder/kmeansOnline.scala
+++ src/main/scala/thunder/kmeansOnline.scala
@@ -59,7 +59,7 @@ object kmeansOnline {
 
   def printToImage(rdd: RDD[Double], width: Int, height: Int, fileName: String): Unit = {
     val nPixels = width * height
-    val R, G, B = rdd.collect().map(_ / 100).map(_ + 255 / 2).map(_ toInt).map(x => if (x < 0) {
+    val R, G, B = rdd.collect().map(_ / 1000).map(_ + 255 / 2).map(_ toInt).map(x => if (x < 0) {
       0
     } else if (x > 255) {
       255
