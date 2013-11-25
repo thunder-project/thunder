c67704ea5bd991b6ff5354d96861ec73dd34ffce
freeman <the.freeman.lab@gmail.com>
2013-11-25 01:15:39 -0500

    Changed number of PCs

--- python/thunder/regression/regression.py
+++ python/thunder/regression/regression.py
@@ -42,7 +42,7 @@ saveout(stats,outputDir,"stats","matlab")
 #saveout(diff,outputDir,"diff","matlab")
 
 # do PCA
-comps,latent,scores = svd1(betas.map(lambda x : x[0]),3)
+comps,latent,scores = svd1(betas.map(lambda x : x[0]),5)
 
 # write results
 saveout(comps,outputDir,"comps","matlab")
