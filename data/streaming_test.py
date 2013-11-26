1091d541db93f533f1e078118825257bc3789371
freeman <the.freeman.lab@gmail.com>
2013-11-26 01:45:35 -0500

    Sort files by modification date when testing streaming to preserve order

--- data/streaming_test.py
+++ data/streaming_test.py
@@ -15,7 +15,7 @@ import sys, os, time, glob;
 srcPath = str(sys.argv[1])
 targetPath = str(sys.argv[2])
 waitTime = float(sys.argv[3])
-files = glob.glob(srcPath+"*")
+files = sorted(glob.glob(srcPath+"*"),key=os.path.getmtime)
 count = 1
 for f in files:
 	cmd = "scp " + f + " " + targetPath 
