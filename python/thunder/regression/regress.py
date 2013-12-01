a43c3d1089dd35fa93cae365c87fa689bac8696c
freeman <the.freeman.lab@gmail.com>
2013-12-01 13:22:04 -0500

    Renamed regression function -> regress to avoid conflict with module name

---
+++ python/thunder/regression/regress.py
@@ -0,0 +1,57 @@
+# regress <master> <dataFile> <modelFile> <outputDir> <regressMode>
+#
+# fit a regression model
+#
+# regressMode - form of regression (mean, linear, bilinear)
+#
+
+import sys
+import os
+from numpy import *
+from scipy.linalg import *
+from pyspark import SparkContext
+from thunder.util.dataio import *
+from thunder.regression.util import *
+from thunder.factorization.util import *
+
+argsIn = sys.argv[1:]
+if len(argsIn) < 5:
+    print >> sys.stderr, \
+    "usage: regress <master> <dataFile> <modelFile> <outputDir> <regressMode>"
+    exit(-1)
+
+# parse inputs
+sc = SparkContext(argsIn[0], "regress")
+dataFile = str(argsIn[1])
+modelFile = str(argsIn[2])
+outputDir = str(argsIn[3]) + "-regress"
+regressMode = str(argsIn[4])
+if not os.path.exists(outputDir) : os.makedirs(outputDir)
+
+# parse data
+lines = sc.textFile(dataFile)
+data = parse(lines, "sub").cache()
+
+# create model
+model = regressionModel(modelFile,regressMode)
+
+# do regression
+betas = regressionFit(data,model).cache()
+
+# get statistics
+stats = betas.map(lambda x : x[1])
+saveout(stats,outputDir,"stats","matlab")
+
+# do PCA
+comps,latent,scores = svd1(betas.map(lambda x : x[0]),2)
+saveout(comps,outputDir,"comps","matlab")
+saveout(latent,outputDir,"latent","matlab")
+saveout(scores,outputDir,"scores","matlab",2)
+
+# compute trajectories from raw data
+traj = regressionFit(data,model,comps)
+saveout(traj,outputDir,"traj","matlab")
+
+# get simple measure of response strength
+r = data.map(lambda x : norm(x-mean(x)))
+saveout(r,outputDir,"r","matlab")
