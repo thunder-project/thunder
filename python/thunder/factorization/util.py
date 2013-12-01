be27f10211b416758c25bb28f5c7e419abe17830
freeman <the.freeman.lab@gmail.com>
2013-11-30 20:32:42 -0500

    Fixed syntax in iterative update for SVD

--- python/thunder/factorization/util.py
+++ python/thunder/factorization/util.py
@@ -25,41 +25,35 @@ def svd1(data,k,meanSubtract=1) :
 # def svd2(data,k,meanSubtract=1) :
 
 def svd2(data,k,meanSubtract=1) :
-
 	def randomVector(ind,seed,k) :
-
 		random.seed(ind*100000*seed)
 		r = random.rand(1,k)
 		return r
 
 	m = len(data.first()[1])
 
-  v = data.map(lambda x,y : randomVector(x,seed1,k))
-  u = random.randn(k,m)
+	v = data.map(lambda x,y : randomVector(x,seed1,k))
+	u = random.randn(k,m)
+	nIter = 5
+	iIter = 1
+	# fixed initialization
+	#var u = factory2D.make(sc.textFile("data/h0.txt").map(parseLine _).toArray())
+	#var w = sc.textFile("data/w0.txt").map(parseVector2 _)
+	while (iter < nIter) :
+		# goal is to solve R = VU subject to U,V > 0
+		# by iteratively updating U and V with least squares and clipping
+    
+		# precompute inv(V' * V)
+		vinv = inv(v.map(lambda x : outer(x,x)).reduce(lambda x,y : (x+y)))
 
-  nIter = 5
-  iIter = 1
+		# update U using least squares row-wise with inv(V' * V) * V * R (same as pinv(V) * R)
+		u = data.join(v.map(lambda x : dot(vinv,x))).mapValues(lambda x,y : outer(x,y)).reduce(lambda x,y: x + y)
 
-  # fixed initialization
-  #var u = factory2D.make(sc.textFile("data/h0.txt").map(parseLine _).toArray())
-  #var w = sc.textFile("data/w0.txt").map(parseVector2 _)
+		# precompute pinv(U)
+		uinv = tranpose(inv(transpose(u)))
 
-  while (iter < nIter) :
-
-    # goal is to solve R = VU subject to U,V > 0
-    # by iteratively updating U and V with least squares and clipping
-
-    # precompute inv(V' * V)
-    vinv = inv(v.map(lambda x : outer(x,x)).reduce(lambda x,y : (x+y))
-
-    # update U using least squares row-wise with inv(V' * V) * V * R (same as pinv(V) * R)
-    u = data.join(v.map(lambda x : dot(vinx,x))).mapValues(lambda x,y : outer(x,y)).reduce(lambda x,y: x + y)
-
-    # precompute pinv(U)
-    uinv = tranpose(inv(transpose(u)))
-
-    # update V using least squares row-wise with R * pinv(U)
-    v = data.mapValues(lambda x : dot(transpose(uinv),x))
-
-    iter += 1
+		# update V using least squares row-wise with R * pinv(U)
+		v = data.mapValues(lambda x : dot(transpose(uinv),x))
+    
+		iter += 1
 
