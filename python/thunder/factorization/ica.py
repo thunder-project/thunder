225a24e514c22356e939a6649c375fc74431f8a9
freeman <the.freeman.lab@gmail.com>
2013-12-08 16:07:47 -0500

    Using accumulators for updates

--- python/thunder/factorization/ica.py
+++ python/thunder/factorization/ica.py
@@ -12,16 +12,22 @@
 
 import sys
 import os
-from numpy import random, sqrt, zeros, real, dot, outer, diag, transpose
+from numpy import random, sqrt, zeros, real, dot, outer, diag, transpose, shape
 from scipy.linalg import sqrtm, inv, orth
 from scipy.io import loadmat
 from thunder.util.dataio import parse, saveout
 from thunder.factorization.util import svd1, svd2, svd3, svd4
 from pyspark import SparkContext
+from pyspark.accumulators import AccumulatorParam
 
 
-def outerSum(iterator):
-    yield sum(outer(x, y) for x, y in iterator)
+class MatrixAccumulatorParam(AccumulatorParam):
+    def zero(self, value):
+        return zeros(shape(value))
+
+    def addInPlace(self, val1, val2):
+        val1 += val2
+        return val1
 
 argsIn = sys.argv[1:]
 if len(argsIn) < 5:
@@ -43,7 +49,7 @@ data = parse(lines, "raw", None, [150, 1000]).cache()
 n = data.count()
 
 # reduce dimensionality
-#comps, latent, scores = svd4(sc, data, k, 0)
+comps, latent, scores = svd4(sc, data, k, 0)
 
 # whiten data
 #whtMat = real(dot(inv(diag(sqrt(latent))), comps))
@@ -59,8 +65,8 @@ wht = data.map(lambda x: dot(whtMat, x))
 #saveout(unwhtMat, outputDir, "unwhtMat", "matlab")
 
 # do multiple independent component extraction
-#B = orth(random.randn(k, c))
-B = loadmat(outputDir + "/B.mat")['B']
+B = orth(random.randn(k, c))
+#B = loadmat(outputDir + "/B.mat")['B']
 Bold = zeros((k, c))
 iterNum = 0
 minAbsCos = 0
@@ -68,11 +74,21 @@ tol = 0.000001
 iterMax = 1000
 errVec = zeros(iterMax)
 
+
+global Bnew
+
+def outerSum(x, y):
+    global Bnew
+    Bnew += outer(x, y)
+
 while (iterNum < iterMax) & ((1 - minAbsCos) > tol):
     iterNum += 1
     # update rule for pow3 nonlinearity (TODO: add other nonlins)
     #B = wht.map(lambda x: (x, dot(x, B) ** 3)).mapPartitions(outerSum).reduce(lambda x, y: x + y) / n - 3 * B
-    B = wht.map(lambda x: outer(x, dot(x, B) ** 3)).reduce(lambda x, y: x + y) / n - 3 * B
+    Bnew = sc.accumulator(zeros((k, c)), MatrixAccumulatorParam())
+    wht.map(lambda x: (x, dot(x, B) ** 3)).foreach(lambda x: outerSum(x[0], x[1]))
+    B = Bnew.value / n - 3 * B
+    #B = wht.map(lambda x: outer(x, dot(x, B) ** 3)).reduce(lambda x, y: x + y) / n - 3 * B
     # orthognalize
     B = dot(B, real(sqrtm(inv(dot(transpose(B), B)))))
     # evaluate error
