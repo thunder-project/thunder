a6076943ae94440f11ecb3bfeee86ee99b59d55b
freeman <the.freeman.lab@gmail.com>
2014-01-04 12:16:36 -0500

    Added clustering package to setup.py, set to version 0.1

--- python/setup.py
+++ python/setup.py
@@ -2,11 +2,11 @@
 
 from setuptools import setup
 
-setup(name='Thunder',
-      version='1.0',
+setup(name='thunder',
+      version='0.1',
       description='Neural data analysis in Spark',
       author='The Freeman Lab',
       author_email='the.freeman.lab@gmail.com',
       url='https://github.com/freeman-lab/thunder',
-      packages=['thunder','thunder.regression','thunder.factorization','thunder.summary','thunder.util'],
+      packages=['thunder','thunder.regression','thunder.factorization','thunder.clustering','thunder.summary','thunder.util'],
      )
