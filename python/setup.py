f65abde775fc2cc6ae1db419b3880a996bcd91ed
freeman <the.freeman.lab@gmail.com>
2014-01-04 13:46:25 -0500

    Fixed formatting on setup.py

--- python/setup.py
+++ python/setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup
 
 setup(name='thunder',
-      version='0.1',
-      description='Neural data analysis in Spark',
-      author='The Freeman Lab',
-      author_email='the.freeman.lab@gmail.com',
-      url='https://github.com/freeman-lab/thunder',
-      packages=['thunder','thunder.regression','thunder.factorization','thunder.clustering','thunder.summary','thunder.util'],
-     )
+    version='0.1',
+    description='Neural data analysis in Spark',
+    author='The Freeman Lab',
+    author_email='the.freeman.lab@gmail.com',
+    url='https://github.com/freeman-lab/thunder',
+    packages=['thunder','thunder.regression','thunder.factorization','thunder.clustering','thunder.summary','thunder.util'],
+    )
