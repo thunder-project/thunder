86f4a039c7f9a5d9fd7d487c0b8471013a63a733
freeman <the.freeman.lab@gmail.com>
2014-01-08 00:45:44 -0500

    Changed kmeans clustering
    - Dropped calculation of distances
    - Made max iterations and tolerance optional arguments

--- python/thunder/clustering/kmeans.py
+++ python/thunder/clustering/kmeans.py
@@ -1,87 +1,78 @@
-# perform kmeans clustering
-#
-# example:
-# pyspark kmeans.py local data/iris.txt results 2 euclidean
-
 import os
 import argparse
-from numpy import dot, mean
-from numpy.linalg import norm
+import glob
+from numpy import sum
 from thunder.util.dataio import saveout, parse
 from pyspark import SparkContext
 
 
-def closestPoint(p, centers, dist):
-    bestIndex = 0
+def closestpoint(p, centers):
+    """return the index of the closest point in centers to p"""
+
+    bestindex = 0
     closest = float("+inf")
     for i in range(len(centers)):
-        if dist == 'euclidean':
-            tempDist = sum((p - centers[i]) ** 2)
-        if dist == 'corr':
-            tempDist = max(1 - dot(p, centers[i]), 0)
-        if tempDist < closest:
-            closest = tempDist
-            bestIndex = i
-    return bestIndex, closest
+        tempdist = sum((p - centers[i]) ** 2)
+        if tempdist < closest:
+            closest = tempdist
+            bestindex = i
+    return bestindex
 
 
-def kmeans(data, k, dist):
+def kmeans(data, k, maxiter=20, tol=0.001):
+    """perform kmeans clustering
 
-    if dist == 'corr':
-        data = data.map(lambda x: (x - mean(x)) / norm(x)).cache()
+    arguments:
+    data - RDD of data points
+    k - number of clusters
+    maxiter - maximum number of iterations (default = 20)
+    tol - change tolerance for stopping algorithm (default = 0.001)
 
+    returns:
+    labels - RDD with labels for each data point
+    centers - array of cluster centroids
+    """
     centers = data.take(k)
 
-    convergeDist = 0.001
-    tempDist = 1.0
-    iteration = 0
-    mxIteration = 100
+    tempdist = 1.0
+    iter = 0
 
-    while (tempDist > convergeDist) & (iteration < mxIteration):
-        if dist == 'corr':
-            centers = map(lambda x: x / norm(x), centers)
-        closest = data.map(lambda p: (closestPoint(p, centers, dist)[0], (p, 1)))
-        pointStats = closest.reduceByKey(lambda (x1, y1), (x2, y2): (x1 + x2, y1 + y2))
-        newPoints = pointStats.map(lambda (x, (y, z)): (x, y / z)).collect()
-        tempDist = sum(sum((centers[x] - y) ** 2) for (x, y) in newPoints)
+    while (tempdist > tol) & (iter < maxiter):
+        closest = data.map(lambda p: (closestpoint(p, centers), (p, 1)))
+        pointstats = closest.reduceByKey(lambda (x1, y1), (x2, y2): (x1 + x2, y1 + y2))
+        newpoints = pointstats.map(lambda (x, (y, z)): (x, y / z)).collect()
+        tempdist = sum(sum((centers[x] - y) ** 2) for (x, y) in newpoints)
 
-        for (i, j) in newPoints:
+        for (i, j) in newpoints:
             centers[i] = j
 
-        iteration += 1
+        iter += 1
 
-    if dist == 'corr':
-        centers = map(lambda x: x / norm(x), centers)
+    labels = data.map(lambda p: closestpoint(p, centers))
 
-    labels = data.map(lambda p: closestPoint(p, centers, dist)[0])
-    dists = data.map(lambda p: closestPoint(p, centers, dist)[1])
-    normDists = data.map(lambda p: closestPoint((p - mean(p))/norm(p), centers, 'corr')[1])
-
-    return labels, centers, dists, normDists
+    return labels, centers
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="do kmeans clustering")
     parser.add_argument("master", type=str)
-    parser.add_argument("dataFile", type=str)
-    parser.add_argument("dataMode", choices=("raw", "dff", "sub"), help="form of data preprocessing")
-    parser.add_argument("outputDir", type=str)
+    parser.add_argument("datafile", type=str)
+    parser.add_argument("outputdir", type=str)
     parser.add_argument("k", type=int)
-    parser.add_argument("dist", choices=("euclidean", "correlation"),
-                        help="distance metric for kmeans")
+    parser.add_argument("--maxiter", type=float, default=20, required=False)
+    parser.add_argument("--tol", type=float, default=0.001, required=False)
+    parser.add_argument("--preprocess", choices=("raw", "dff", "sub"), default="raw", required=False)
 
     args = parser.parse_args()
     egg = glob.glob(os.environ['THUNDER_EGG'] + "*.egg")
     sc = SparkContext(args.master, "kmeans", pyFiles=egg)
 
-    lines = sc.textFile(args.dataFile)
-    data = parse(lines, args.dataMode).cache()
+    lines = sc.textFile(args.datafile)
+    data = parse(lines, args.preprocess).cache()
 
-    labels, centers, dists, normDists = kmeans(data, args.k, args.dist)
+    labels, centers = kmeans(data, k=args.k, maxiter=args.maxiter, tol=args.tol)
 
-    outputDir = args.outputDir + "-kmeans"
-    if not os.path.exists(outputDir):
-        os.makedirs(outputDir)
-    saveout(labels, outputDir, "labels", "matlab")
-    saveout(dists, outputDir, "dists", "matlab")
-    saveout(centers, outputDir, "centers", "matlab")
-    saveout(normDists, outputDir, "normDists", "matlab")
+    outputdir = args.outputdir + "-kmeans"
+    if not os.path.exists(outputdir):
+        os.makedirs(outputdir)
+    saveout(labels, outputdir, "labels", "matlab")
+    saveout(centers, outputdir, "centers", "matlab")
