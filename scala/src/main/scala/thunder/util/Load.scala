c598ec536b66922eba1619fcebcb1e77413145ba
freeman <the.freeman.lab@gmail.com>
2014-02-21 00:20:24 -0500

    Expanded options for loading streaming and batch data

--- scala/src/main/scala/thunder/util/Load.scala
+++ scala/src/main/scala/thunder/util/Load.scala
@@ -6,7 +6,7 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.streaming.StreamingContext
-import org.apache.spark.streaming.DStream
+import org.apache.spark.streaming.dstream.DStream
 
 import scala.math.max
 import scala.math.ceil
@@ -19,7 +19,14 @@ object Load {
    * @param nKeys Number of keys per data point
    */
   case class DataLoader(nKeys: Int) {
-    def get(line: String): (Array[Int], Array[Double]) = {
+    def get(line: String): Array[Double] = {
+      val parts = line.split(' ')
+      val value = parts.slice(nKeys, parts.length).map(_.toDouble)
+      value
+
+    }
+
+    def getWithKeys(line: String): (Array[Int], Array[Double]) = {
       val parts = line.split(' ')
       val key = parts.slice(0,nKeys).map(_.toDouble.toInt)
       val value = parts.slice(nKeys, parts.length).map(_.toDouble)
@@ -47,7 +54,7 @@ object Load {
   }
 
   /**
-   * Get maximum dimensions of data based on keys
+   * Get maximum dimensions of data based on keys in an RDD
    *
    * @param data: RDD of data points as key value pairs
    */
@@ -56,14 +63,14 @@ object Load {
   }
 
   /**
-   * Convert subscript indexing to linear indexing
+   * Convert subscript indexing to linear indexing in an RDD
    *
    * @param data RDD of data points as key value pairs
    * @param dims Dimensions for subscript indices
    * @return RDD of data points with linear indices as keys
    */
-  def subToInd(data: DStream[(Array[Int], Array[Double])], dims: Array[Int]):
-    DStream[(Int, Array[Double])] = dims.length match {
+  def subToInd(data: RDD[(Array[Int], Array[Double])], dims: Array[Int]):
+    RDD[(Int, Array[Double])] = dims.length match {
       case 1 => data.map{case (k, v) => (k(0), v)}
       case _ =>
         val dimprod = dims.map(x => Range(1, x + 1).reduce(_*_)).dropRight(1)
@@ -73,24 +80,82 @@ object Load {
   }
 
   /**
-   * Convert linear indexing to subscript indexing
+   * Convert linear indexing to subscript indexing in an RDD
    *
    * @param data RDD of data points as key value pairs
    * @param dims Dimensions for subscript indices
    * @return RDD of data points with subscript indices as keys
    */
-  def indToSub(data: DStream[(Array[Int], Array[Double])], dims: Array[Int]):
-    DStream[(Array[Int], Array[Double])] = dims.length match {
-    case 1 => data
+  def indToSub(data: RDD[(Int, Array[Double])], dims: Array[Int]):
+    RDD[(Array[Int], Array[Double])] = dims.length match {
+    case 1 => data.map{case (k, v) => (Array(k), v)}
     case _ =>
       val dimprod = dims.zip(Array.concat(Array(1), dims.map(x => Range(1, x + 1).reduce(_*_)).dropRight(1)))
       data.map{case (k, v) =>
-        (dimprod.map{case (x, y) => ((ceil(k(0).toDouble / y) - 1) % x + 1).toInt}, v)
+        (dimprod.map{case (x, y) => ((ceil(k.toDouble / y) - 1) % x + 1).toInt}, v)
+      }
+  }
+
+  /**
+   * Convert subscript indexing to linear indexing in a DStream
+   *
+   * @param data DStream of data points as key value pairs
+   * @param dims Dimensions for subscript indices
+   * @return DStream of data points with linear indices as keys
+   */
+  def subToIndStreaming(data: DStream[(Array[Int], Array[Double])], dims: Array[Int]):
+  DStream[(Int, Array[Double])] = dims.length match {
+    case 1 => data.map{case (k, v) => (k(0), v)}
+    case _ =>
+      val dimprod = dims.map(x => Range(1, x + 1).reduce(_*_)).dropRight(1)
+      data.map{case (k, v) =>
+        (dimprod.zip(k.drop(1)).map{case (x, y) => (x - 1) * y}.sum + k(0), v)
+      }
+  }
+
+  /**
+   * Convert linear indexing to subscript indexing in a DStream
+   *
+   * @param data DStream of data points as key value pairs
+   * @param dims Dimensions for subscript indices
+   * @return DStream of data points with subscript indices as keys
+   */
+  def indToSubStreaming(data: DStream[(Int, Array[Double])], dims: Array[Int]):
+  DStream[(Array[Int], Array[Double])] = dims.length match {
+    case 1 => data.map{case (k, v) => (Array(k), v)}
+    case _ =>
+      val dimprod = dims.zip(Array.concat(Array(1), dims.map(x => Range(1, x + 1).reduce(_*_)).dropRight(1)))
+      data.map{case (k, v) =>
+        (dimprod.map{case (x, y) => ((ceil(k.toDouble / y) - 1) % x + 1).toInt}, v)
       }
   }
 
   /**
    * Load data from a text file with format
+   * <t1> <t2> ...
+   * where <t1> <t2> ... are the data values (Double)
+   *
+   * @param sc SparkContext
+   * @param dir Directory to the input data files
+   * @param PreProcessMethod Method for pre processing data (default = "raw")
+   * @return An RDD of data, each with two elements: the first has a key (Array[Int]),
+   *         and the second has the data values (an Array[Double])
+   */
+  def loadData(sc: SparkContext,
+                       dir: String,
+                       PreProcessMethod: String = "raw"): RDD[Array[Double]] = {
+    val Loader = new DataLoader(0)
+
+    if (PreProcessMethod != "raw") {
+      val PreProcessor = new DataPreProcessor(PreProcessMethod)
+      sc.textFile(dir).map(Loader.get).map(PreProcessor.get)
+    } else {
+      sc.textFile(dir).map(Loader.get)
+    }
+  }
+
+  /**
+   * Load keyed data from a text file with format
    * <k1> <k2> ... <t1> <t2> ...
    * where <k1> <k2> ... are keys (Int) and <t1> <t2> ... are the data values (Double)
    *
@@ -101,23 +166,39 @@ object Load {
    * @return An RDD of data, each with two elements: the first has a key (Array[Int]),
    *         and the second has the data values (an Array[Double])
    */
-  def loadData(sc: SparkContext,
+  def loadDataWithKeys(sc: SparkContext,
                dir: String,
                PreProcessMethod: String = "raw",
                nKeys: Int = 3): RDD[(Array[Int], Array[Double])] = {
     val Loader = new DataLoader(nKeys)
-    var data = sc.textFile(dir).map(Loader.get)
 
     if (PreProcessMethod != "raw") {
       val PreProcessor = new DataPreProcessor(PreProcessMethod)
-      data = data.mapValues(PreProcessor.get)
+      sc.textFile(dir).map(Loader.getWithKeys).mapValues(PreProcessor.get)
+    } else {
+      sc.textFile(dir).map(Loader.getWithKeys)
     }
-
-    data
   }
 
   /**
    * Load streaming data from text files with format
+   * <t1> <t2> ...
+   * Where <t1> <t2> are the data values (Double)
+   *
+   * @param ssc StreamingContext
+   * @param dir Directory to the input data files
+   * @return A DStream of data, each with two elements: the first has the keys,
+   *         and the second has the data values (an array of Double)
+   */
+  def loadStreamingData (ssc: StreamingContext,
+                         dir: String): DStream[Array[Double]] = {
+    val Loader = new DataLoader(0)
+    ssc.textFileStream(dir).map(Loader.get)
+
+  }
+
+  /**
+   * Load keyed streaming data from text files with format
    * <k1> <k2> ... <t1> <t2> ...
    * where <k1> <k2> ... are keys (Int) and <t1> <t2> ... are the data values (Double)
    * If multiple keys are provided (e.g. x, y, z), they are converted to linear indexing
@@ -129,12 +210,12 @@ object Load {
    * @return A DStream of data, each with two elements: the first has the keys,
    *         and the second has the data values (an array of Double)
    */
-  def loadStreamingData (ssc: StreamingContext,
+  def loadStreamingDataWithKeys (ssc: StreamingContext,
                          dir: String,
                          nKeys: Int = 1,
                          dims: Array[Int] = Array(1)): DStream[(Int, Array[Double])] = {
     val Loader = new DataLoader(nKeys)
-    subToInd(ssc.textFileStream(dir).map(Loader.get), dims)
+    subToIndStreaming(ssc.textFileStream(dir).map(Loader.getWithKeys), dims)
 
   }
 
