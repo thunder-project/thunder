10f77b3f9b6250e0798353448274ee7d1360ad66
freeman <the.freeman.lab@gmail.com>
2014-02-23 18:08:16 -0500

    Added option for loading labeled streaming data in the MLlib format

--- scala/src/main/scala/thunder/util/Load.scala
+++ scala/src/main/scala/thunder/util/Load.scala
@@ -7,6 +7,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.mllib.regression.LabeledPoint
 
 import scala.math.max
 import scala.math.ceil
@@ -15,23 +16,32 @@ object Load {
 
   /**
    * Class for loading lines of a data file
+   * supporting a variety of formats
    *
-   * @param nKeys Number of keys per data point
+   * @param nKeys Number of integer keys per data point
    */
-  case class DataLoader(nKeys: Int) {
+  case class DataLoader(nKeys: Int = 0) {
+
+    /** nKeys per line, but only keep the values */
     def get(line: String): Array[Double] = {
       val parts = line.split(' ')
       val value = parts.slice(nKeys, parts.length).map(_.toDouble)
       value
-
     }
-
+    /** nKeys per line, keep both */
     def getWithKeys(line: String): (Array[Int], Array[Double]) = {
       val parts = line.split(' ')
       val key = parts.slice(0,nKeys).map(_.toDouble.toInt)
       val value = parts.slice(nKeys, parts.length).map(_.toDouble)
       (key, value)
+    }
 
+    /** Single label per line, separated by a comma */
+    def getWithLabels(line: String): LabeledPoint = {
+      val parts = line.split(',')
+      val label = parts(0).toDouble
+      val features = parts(1).trim().split(' ').map(_.toDouble)
+      LabeledPoint(label, features)
     }
   }
 
@@ -48,7 +58,7 @@ object Load {
           x.map(_-mean)
         case "dff" =>
           val mean = x.sum / x.length
-          return x.map{ x =>
+          x.map{ x =>
             (x - mean) / (mean + 0.1) }
     }
   }
@@ -137,17 +147,17 @@ object Load {
    *
    * @param sc SparkContext
    * @param dir Directory to the input data files
-   * @param PreProcessMethod Method for pre processing data (default = "raw")
+   * @param preProcessMethod Method for pre processing data (default = "raw")
    * @return An RDD of data, each with two elements: the first has a key (Array[Int]),
    *         and the second has the data values (an Array[Double])
    */
   def loadData(sc: SparkContext,
                        dir: String,
-                       PreProcessMethod: String = "raw"): RDD[Array[Double]] = {
+                       preProcessMethod: String = "raw"): RDD[Array[Double]] = {
     val Loader = new DataLoader(0)
 
-    if (PreProcessMethod != "raw") {
-      val PreProcessor = new DataPreProcessor(PreProcessMethod)
+    if (preProcessMethod != "raw") {
+      val PreProcessor = new DataPreProcessor(preProcessMethod)
       sc.textFile(dir).map(Loader.get).map(PreProcessor.get)
     } else {
       sc.textFile(dir).map(Loader.get)
@@ -202,7 +212,7 @@ object Load {
    * <k1> <k2> ... <t1> <t2> ...
    * where <k1> <k2> ... are keys (Int) and <t1> <t2> ... are the data values (Double)
    * If multiple keys are provided (e.g. x, y, z), they are converted to linear indexing
-
+   *
    * @param ssc StreamingContext
    * @param dir Directory to the input data files
    * @param nKeys Number of keys per data point
@@ -216,6 +226,24 @@ object Load {
                          dims: Array[Int] = Array(1)): DStream[(Int, Array[Double])] = {
     val Loader = new DataLoader(nKeys)
     subToIndStreaming(ssc.textFileStream(dir).map(Loader.getWithKeys), dims)
+
+  }
+
+  /**
+   * Load labeled streaming data from text files with format
+   * <label>, <t1> <t2> ...
+   * where <label> is the label and <t1> <t2> ... are the data values (Double),
+   * note the comma after the label
+   *
+   * @param ssc StreamingContext
+   * @param dir Directory to the input data files
+   * @return A DStream of data, each with two elements: the first has the keys,
+   *         and the second has the data values (an array of Double)
+   */
+  def loadStreamingLabeledData (ssc: StreamingContext,
+                                 dir: String): DStream[LabeledPoint] = {
+    val Loader = new DataLoader()
+    ssc.textFileStream(dir).map(Loader.getWithLabels)
 
   }
 
