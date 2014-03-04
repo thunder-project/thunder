7d77d770b929f10510b8e9a922132c0cabe8a504
freeman <the.freeman.lab@gmail.com>
2014-03-03 18:47:22 -0700

    Initial commit of Save class

--- scala/src/main/scala/thunder/util/Save.scala
+++ scala/src/main/scala/thunder/util/Save.scala
@@ -2,6 +2,53 @@
 
 package thunder.util
 
+import org.apache.spark.rdd.RDD
+
+
 object Save {
+
+  abstract class DataSaver(var directory: String) {
+
+    def write(rdd: RDD[Int])
+
+  }
+
+  class ImageSaver(override var directory: String) extends DataSaver(directory) {
+
+    var colorMode = new String
+
+    def setColorMode(colorMode: String): ImageSaver = {
+      this.colorMode = colorMode
+      this
+    }
+
+    def write(rdd: RDD[Int]) = {
+      rdd.collect()
+
+    }
+
+  }
+
+  class TextSaver(override var directory: String) extends DataSaver(directory) {
+
+    def write(rdd: RDD[Int]) = {
+      rdd.collect()
+
+    }
+
+  }
+
+  def save(rdd: RDD[Int], format: String, directory: String) {
+
+    format match {
+      case "text" =>
+        val saver = new TextSaver(directory)
+      case "image" =>
+        val saver = new ImageSaver(directory)
+        saver.setColorMode("test")
+    }
+
+
+  }
 
 }
