1c8afa0ee23a0cf31fdbb6d4ea5caefc35db33b8
freeman <the.freeman.lab@gmail.com>
2014-04-04 15:02:32 -0400

    Expanded number of automatic imports

--- helper/thunder-startup.py
+++ helper/thunder-startup.py
@@ -4,8 +4,20 @@ import glob
 import os
 egg = glob.glob(os.path.join(os.environ['THUNDER_EGG'], "*.egg"))
 sc.addPyFile(egg[0])
+
 from thunder.util.load import load, getdims
 from thunder.util.save import save
+
+from thunder.clustering.kmeans import kmeans, closestpoint
+
 from thunder.regression.regress import regress
+from thunder.regression.util import RegressionModel, TuningModel
+
 from thunder.factorization.pca import pca
+from thunder.factorization.ica import ica
+from thunder.factorization.util import svd
+
 from thunder.sigprocessing.stats import stats
+from thunder.sigprocessing.localcorr import localcorr
+from thunder.sigprocessing.query import query
+from thunder.sigprocessing.util import FourierMethod, StatsMethod, QueryMethod, CrossCorrMethod
