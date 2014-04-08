f83d5496aa77ab1c299eb709e9d114dfcb7ef827
freeman <the.freeman.lab@gmail.com>
2014-04-08 14:14:42 -0400

    Added mass univariate classification analysis
    - Assess how well individual neural signals predict a target variable
    - Users sklearn classification methods

---
+++ python/thunder/classification/classify.py
@@ -0,0 +1,53 @@
+import os
+import argparse
+import glob
+from thunder.classification.util import MassUnivariateClassifier
+from thunder.util.load import load
+from thunder.util.save import save
+from pyspark import SparkContext
+
+
+def classify(data, params, classifymode, set=None, cv=0):
+    """Perform mass univariate classification
+
+    :param data: RDD of data points as key value pairs
+    :param params: string with file location, or dictionary of parameters for classification
+    :param classifymode: form of classifier ("naivebayes")
+    :param set: set of features to use for classification
+    :param cv: number of cross validation folds (0 for none)
+
+    :return perf: performance
+    """
+    # create classifier
+    clf = MassUnivariateClassifier.load(params, classifymode, cv)
+
+    # do classification
+    perf = clf.classify(data, set)
+
+    return perf
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="fit a regression model")
+    parser.add_argument("master", type=str)
+    parser.add_argument("datafile", type=str)
+    parser.add_argument("paramfile", type=str)
+    parser.add_argument("outputdir", type=str)
+    parser.add_argument("classifymode", choices="naivebayes", help="form of classifier")
+    parser.add_argument("--preprocess", choices=("raw", "dff", "dff-highpass", "sub"), default="raw", required=False)
+
+    args = parser.parse_args()
+
+    sc = SparkContext(args.master, "classify")
+
+    if args.master != "local":
+        egg = glob.glob(os.path.join(os.environ['THUNDER_EGG'], "*.egg"))
+        sc.addPyFile(egg[0])
+
+    data = load(sc, args.datafile, args.preprocess)
+
+    perf = classify(data, args.paramfile, args.classifymode)
+
+    outputdir = args.outputdir + "-classify"
+
+    save(perf, outputdir, "perf", "matlab")
---
+++ python/thunder/classification/util.py
@@ -0,0 +1,97 @@
+from numpy import in1d, zeros, array, size
+from scipy.io import loadmat
+from sklearn.naive_bayes import GaussianNB
+from sklearn import cross_validation
+
+
+class MassUnivariateClassifier(object):
+    """Class for loading and classifying with classifiers"""
+
+    def __init__(self, paramfile):
+        """Initialize classifier using parameters derived from a Matlab file,
+        or a python dictionary. At a minimum, must contain a "labels" field, with the
+        label to classify at each time point. Can additionally include fields for
+        "features" (which feature was present at each time point)
+        and "samples" (which sample was present at each time point)
+
+        :param paramfile: string of filename, or dictionary, containing parameters
+        """
+        if type(paramfile) is str:
+            params = loadmat(paramfile, squeeze_me=True)
+        elif type(paramfile) is dict:
+            params = paramfile
+        else:
+            raise TypeError("Parameters for classification must be provided as string with file location, or dictionary")
+
+        self.labels = params['labels']
+
+        if 'features' in params:
+            self.features = params['features']
+            self.nfeatures = len(list(set(self.features.flatten())))
+            self.samples = params['samples']
+            self.sampleids = list(set(self.samples.flatten()))
+            self.nsamples = len(self.sampleids)
+        else:
+            self.nfeatures = 1
+            self.nsamples = len(self.labels)
+
+    @staticmethod
+    def load(paramfile, classifymode, cv=0):
+        return CLASSIFIERS[classifymode](paramfile, cv)
+
+    def get(self, x, set=None):
+        pass
+
+    def classify(self, data, set=None):
+
+        if self.nfeatures == 1:
+            perf = data.map(lambda x: [self.get(x)])
+        else:
+            if set is None:
+                set = [[self.features[0]]]
+            if type(set) is int:
+                set = [set]
+            for i in set:
+                assert array(in1d(self.features, i)).sum() != 0, "Feature set invalid"
+            perf = data.map(lambda x: map(lambda i: self.get(x, i), set))
+
+        return perf
+
+
+class GaussNaiveBayesClassifier(MassUnivariateClassifier):
+    """Class for gaussian naive bayes classification"""
+
+    def __init__(self, paramfile, cv):
+        """Create classifier
+
+        :param paramfile: string of filename or dictionary with parameters (see MassUnivariateClassifier)
+        :param cv: number of cross validation folds (none if 0)
+        :
+        """
+        MassUnivariateClassifier.__init__(self, paramfile)
+
+        self.cv = cv
+        self.func = GaussianNB()
+
+    def get(self, x, set=None):
+        """Compute classification performance"""
+
+        y = self.labels
+        if self.nfeatures == 1:
+            X = zeros((self.nsamples, 1))
+            X[:, 0] = x
+        else:
+            X = zeros((self.nsamples, size(set)))
+            for i in range(0, self.nsamples):
+                inds = (self.samples == self.sampleids[i]) & (in1d(self.features, set))
+                X[i, :] = x[inds]
+
+        if self.cv > 0:
+            return cross_validation.cross_val_score(self.func, X, y, cv=self.cv).mean()
+        else:
+            ypred = self.func.fit(X, y).predict(X)
+            return array(y == ypred).mean()
+
+CLASSIFIERS = {
+    'gaussnaivebayes': GaussNaiveBayesClassifier
+}
