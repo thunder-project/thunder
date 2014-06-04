6b99c53cc5323171c186de83260d9f4a715a5c60
freeman <the.freeman.lab@gmail.com>
2014-06-04 18:33:29 -0400

    Changed parallelism specification

--- scala/src/main/scala/thunder/streaming/StatefulBinnedStats.scala
+++ scala/src/main/scala/thunder/streaming/StatefulBinnedStats.scala
@@ -163,6 +163,7 @@ object StatefulBinnedStats {
       conf.setSparkHome(System.getenv("SPARK_HOME"))
         .setJars(List("target/scala-2.10/thunder_2.10-0.1.0.jar"))
         .set("spark.executor.memory", "100G")
+        .set("spark.default.parallelism", "100")
     }
 
     /** Create Streaming Context */
