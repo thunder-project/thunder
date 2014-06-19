cacd8248eaf34591bb6e347a41e9bd3f5fe94ba3
freeman <the.freeman.lab@gmail.com>
2014-06-19 14:30:44 -0400

    Minor refactoring
    - Split off a "select" method so other functions can subselect with
    indices without computing averages

--- python/thunder/timeseries/query.py
+++ python/thunder/timeseries/query.py
@@ -19,7 +19,9 @@ class Query(object):
     Parameters
     ----------
     indsfile : str, or array-like (2D)
-        Array of indices, each an array-like of integer indicies
+        Array of indices, each an array-like of integer indices, or
+        filename of a MAT file containing a set of indices as a cell array
+        stored in the variable inds
     """
 
     def __init__(self, indsfile):
@@ -29,6 +31,27 @@ class Query(object):
             inds = indsfile
         self.inds = inds
         self.n = len(inds)
+
+    def select(self, data, i):
+        """Subselect rows of data with a given set of integer indices
+
+        Parameters
+        ----------
+        data : RDD of (int, array) pairs, each array of shape (ncols,)
+            Data to subselect rows from
+
+        i : int
+            Which set of indices to use
+
+        Returns
+        -------
+        subset : RDD of (int, array) pairs
+            Data with subset of rows
+        """
+
+        indsb = data.context.broadcast(self.inds[i])
+        subset = data.filter(lambda (k, _): k in indsb.value)
+        return subset
 
     def calc(self, data):
         """Calculate averages. Keys (tuples) are converted
@@ -50,9 +73,7 @@ class Query(object):
         # loop over indices, averaging time series
         ts = zeros((self.n, len(data.first()[1])))
         for i in range(0, self.n):
-            indsb = data.context.broadcast(self.inds[i])
-            ts[i, :] = data.filter(lambda (k, _): k in indsb.value).map(
-                lambda (k, x): x).sum() / len(self.inds[i])
+            ts[i, :] = self.select(data, i).map(lambda (k, x): x).sum() / len(self.inds[i])
 
         return ts
 
@@ -63,7 +84,7 @@ if __name__ == "__main__":
     parser.add_argument("datafile", type=str)
     parser.add_argument("indsfile", type=str)
     parser.add_argument("outputdir", type=str)
-    parser.add_argument("--preprocess", choices=("raw", "dff", "dff-highpass", "sub"), default="raw", required=False)
+    parser.add_argument("--preprocess", choices=("raw", "dff", "dff-percentile", "dff-highpass", "sub"), default="raw", required=False)
 
     args = parser.parse_args()
 
