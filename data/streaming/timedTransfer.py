c79c43706dd8789e6ad83fb695e83612a4a36ffe
freeman <the.freeman.lab@gmail.com>
2014-06-27 23:45:44 -0400

    Use atomic file transfer via copy + move

--- data/streaming/timedTransfer.py
+++ data/streaming/timedTransfer.py
@@ -6,21 +6,35 @@
 #
 # <streaming_test> srcPath targetPath waitTime
 #
-# example:
-# data/streaming_test.py /groups/ahrens/ahrenslab/Misha/forJeremy_SparkStreamingSample/ /nobackup/freeman/buffer/ 1
 #
 
 import sys, os, time, glob;
 
 srcPath = str(sys.argv[1])
 targetPath = str(sys.argv[2])
+targetPathTmp = os.path.dirname(targetPath) + "_tmp"
+
+# create temporary folder
+if os.path.exists(targetPathTmp):
+	os.rmdir(targetPathTmp)
+os.mkdir(targetPathTmp)
+
+# delete existing files in target folder
+files = sorted(glob.glob(targetPath+"*.bin"))
+for f in files:
+	os.remove(f)
+
 waitTime = float(sys.argv[3])
-files = sorted(glob.glob(srcPath+"*"),key=os.path.getmtime)
+files = sorted(glob.glob(srcPath+"*.bin"))
 count = 1
 for f in files:
-	cmd = "scp " + f + " " + targetPath 
-	os.system(cmd)
-	print('writing file ' +str(count))
+	cmd_cp = "scp " + f + " " + targetPathTmp 
+	os.system(cmd_cp)
+	print('writing file ' + str(count))
+	tmpfile = os.path.join(targetPathTmp, os.path.basename(f))
+	cmd_mv = "mv " + tmpfile + " " + targetPath
+	os.system(cmd_mv)
 	count = count + 1
 	time.sleep(waitTime)
 
+os.rmdir(targetPathTmp)
