078be49ea87d2d6282905e5cef2f995e92c1dbfa
freeman <the.freeman.lab@gmail.com>
2014-07-18 04:57:31 -0400

    Automatically import version numbers

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import thunder
 
-setup(name='thunder',
-    version='0.1.0',
+setup(name='Thunder',
+    version=str(thunder.__version__),
     description='Large-scale neural data analysis in Spark',
     author='The Freeman Lab',
     author_email='the.freeman.lab@gmail.com',
     url='https://github.com/freeman-lab/thunder',
     packages=['thunder','thunder.regression','thunder.factorization','thunder.classification','thunder.clustering','thunder.timeseries','thunder.io','thunder.util','thunder.viz'],
+    long_description=open('../README.rst').read(),
+    license=open('../LICENSE.txt'),
+    install_requires=[open('requirements.txt').read()]
     )
