e981d64b66303aee5118d95eb910f6e261ba7a8d
freeman <the.freeman.lab@gmail.com>
2014-10-06 00:34:21 -0400

    Minimize py4j output during tests

--- python/test/test_utils.py
+++ python/test/test_utils.py
@@ -1,6 +1,7 @@
 import shutil
 import tempfile
 import unittest
+import logging
 from pyspark import SparkContext
 
 
@@ -8,6 +9,8 @@ class PySparkTestCase(unittest.TestCase):
     def setUp(self):
         class_name = self.__class__.__name__
         self.sc = SparkContext('local', class_name)
+        logging.getLogger("py4j").setLevel(logging.WARNING)
+        logging.getLogger("py4j.java_gateway").setLevel(logging.WARNING)
 
     def tearDown(self):
         self.sc.stop()
@@ -20,6 +23,8 @@ class PySparkTestCaseWithOutputDir(PySparkTestCase):
     def setUp(self):
         super(PySparkTestCaseWithOutputDir, self).setUp()
         self.outputdir = tempfile.mkdtemp()
+        logging.getLogger("py4j").setLevel(logging.WARNING)
+        logging.getLogger("py4j.java_gateway").setLevel(logging.WARNING)
 
     def tearDown(self):
         super(PySparkTestCaseWithOutputDir, self).tearDown()
