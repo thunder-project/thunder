521d98558f00f59bf97d05c60837e89478f2d3f2
freeman <the.freeman.lab@gmail.com>
2014-10-08 23:50:48 -0400

    Fixed option in test

--- python/test/test_matrices.py
+++ python/test/test_matrices.py
@@ -36,9 +36,7 @@ class TestTimes(MatrixRDDTestCase):
         mat2 = RowMatrix(self.sc.parallelize([(1, array([7, 8, 9])), (2, array([10, 11, 12]))], 2))
         truth = array([[47, 52, 57], [64, 71, 78], [81, 90, 99]])
         resultA = mat1.times(mat2)
-        resultB = mat1.times(mat2, "accum")
         assert array_equal(resultA, truth)
-        assert array_equal(resultB, truth)
 
     def test_times_array(self):
         mat1 = RowMatrix(self.sc.parallelize([(1, array([1, 2, 3])), (2, array([4, 5, 6]))]))
