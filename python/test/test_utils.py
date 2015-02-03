c6613d1476d22d2a3a66f696c273380c9487f8af
Michael Broxton <broxton@gmail.com>
2015-02-03 10:55:33 -0800

    Addressing code review comments from @poolio.  Conformed to Thunder style guidelines and added an additional unit test.

--- python/test/test_utils.py
+++ python/test/test_utils.py
@@ -55,11 +55,11 @@ def elementwiseStdev(arys):
 
 class TestSerializableDecorator(PySparkTestCase):
 
-    def test_serializable_decorator(self):
+    def testSerializableDecorator(self):
 
         from thunder.utils.decorators import serializable
-        import numpy as np
-        import datetime
+        from numpy import array, all
+        from datetime import datetime
 
         @serializable
         class Visitor(object):
@@ -67,10 +67,10 @@ class TestSerializableDecorator(PySparkTestCase):
                 self.ip = ip_addr
                 self.ua = agent
                 self.referrer= referrer
-                self.test_dict = {'a': 10, 'b': "string", 'c': [1, 2, 3]}
-                self.test_vec = np.array([1,2,3])
-                self.test_array = np.array([[1,2,3],[4,5,6.]])
-                self.time = datetime.datetime.now()
+                self.testDict = {'a': 10, 'b': "string", 'c': [1, 2, 3]}
+                self.testVec = array([1,2,3])
+                self.testArray = array([[1,2,3],[4,5,6.]])
+                self.time = datetime.now()
 
             def __str__(self):
                 return str(self.ip) + " " + str(self.ua) + " " + str(self.referrer) + " " + str(self.time)
@@ -81,20 +81,51 @@ class TestSerializableDecorator(PySparkTestCase):
         # Run the test.  Build an object, serialize it, and recover it.
 
         # Create a new object
-        orig_visitor = Visitor('192.168', 'UA-1', 'http://www.google.com')
+        origVisitor = Visitor('192.168', 'UA-1', 'http://www.google.com')
 
         # Serialize the object
-        pickled_visitor = orig_visitor.serialize(numpy_storage='ascii')
+        pickled_visitor = origVisitor.serialize(numpyStorage='ascii')
 
         # Restore object
-        recov_visitor = Visitor.deserialize(pickled_visitor)
+        recovVisitor = Visitor.deserialize(pickled_visitor)
 
         # Check that the object was reconstructed successfully
-        assert(orig_visitor.ip == recov_visitor.ip)
-        assert(orig_visitor.ua == recov_visitor.ua)
-        assert(orig_visitor.referrer == recov_visitor.referrer)
-        for key in orig_visitor.test_dict.keys():
-            assert(orig_visitor.test_dict[key] == recov_visitor.test_dict[key])
+        assert(origVisitor.ip == recovVisitor.ip)
+        assert(origVisitor.ua == recovVisitor.ua)
+        assert(origVisitor.referrer == recovVisitor.referrer)
+        for key in origVisitor.testDict.keys():
+            assert(origVisitor.testDict[key] == recovVisitor.testDict[key])
 
-        assert(np.all(orig_visitor.test_vec == recov_visitor.test_vec))
-        assert(np.all(orig_visitor.test_array == recov_visitor.test_array))
+        assert(all(origVisitor.testVec == recovVisitor.testVec))
+        assert(all(origVisitor.testArray == recovVisitor.testArray))
+
+    def testNotSerializable(self):
+        '''
+        Unit test to make sure exceptions are thrown if the object contains an
+        unserializable data type.
+        '''
+
+        from thunder.utils.decorators import serializable
+        from numpy import array, all
+        from datetime import datetime
+
+        class SomeOtherClass(object):
+            def __init__(self):
+                someVariable = 3
+
+        @serializable
+        class Visitor(object):
+            def __init__(self):
+                self.refrerenceToUnserializableClass = [ SomeOtherClass() ]
+
+        origVisitor = Visitor()
+
+        # Serialize the object
+        try:
+            pickled_visitor = origVisitor.serialize()   # This should fail
+            assert(False)   # The @serializable wrapped class should have thrown an exception, but didn't!
+        except(TypeError):
+            pass            # If the exception was thrown and caught, the test has passed
+
+
+
--- python/thunder/utils/decorators.py
+++ python/thunder/utils/decorators.py
@@ -1,6 +1,6 @@
 """ Useful decorators that are used throughout the library """
 
-def _isnamedtuple(obj):
+def _isNamedTuple(obj):
   """Heuristic check if an object is a namedtuple."""
   return isinstance(obj, tuple) \
            and hasattr(obj, "_fields") \
@@ -17,19 +17,19 @@ def serializable(cls):
 
       @serializable
       class Visitor():
-          def __init__(self, ip_addr = None, agent = None, referrer = None):
-              self.ip = ip_addr
+          def __init__(self, ipAddr = None, agent = None, referrer = None):
+              self.ip = ipAddr
               self.ua = agent
               self.referrer= referrer
               self.time = datetime.datetime.now()
 
-      orig_visitor = Visitor('192.168', 'UA-1', 'http://www.google.com')
+      origVisitor = Visitor('192.168', 'UA-1', 'http://www.google.com')
 
       #serialize the object
-      pickled_visitor = orig_visitor.serialize()
+      pickledVisitor = origVisitor.serialize()
 
       #restore object
-      recov_visitor = Visitor.deserialize(pickled_visitor)
+      recovVisitor = Visitor.deserialize(pickledVisitor)
 
     Note that this decorator is NOT designed to provide generalized pickling
     capabilities. Rather, it is designed to make it very easy to convert small
@@ -87,7 +87,7 @@ def serializable(cls):
         # ------------------------------------------------------------------------------
         # SERIALIZE()
 
-        def serialize(self, numpy_storage='auto'):
+        def serialize(self, numpyStorage='auto'):
             '''
             Serialize this object to a python dictionary that can easily be converted
             to/from JSON using Python's standard JSON library.
@@ -96,7 +96,7 @@ def serializable(cls):
 
               numpy-storage: choose one of ['auto', 'ascii', 'base64'] (default: auto)
 
-              Use the 'nmupy_storage' argument to select whether numpy arrays
+              Use the 'numpyStorage' argument to select whether numpy arrays
               will be encoded in ASCII (as a list of lists) in Base64 (i.e.
               space efficient binary), or to select automatically (the default)
               depending on the size of the array. Currently the Base64 encoding
@@ -106,101 +106,101 @@ def serializable(cls):
 
               The object encoded as a python dictionary with "JSON-safe" datatypes that is ready to
               be converted to a string using Python's standard JSON library (or another library of
-              your choice.
+              your choice).
             '''
             from collections import namedtuple, Iterable, OrderedDict
-            import numpy as np
+            from numpy import ndarray
 
-            def serialize_recursively(data):
+            def serializeRecursively(data):
                 import datetime
 
                 if data is None or isinstance(data, (bool, int, long, float, basestring)):
                     return data
                 if isinstance(data, list):
-                    return [serialize_recursively(val) for val in data]           # Recurse into lists
+                    return [serializeRecursively(val) for val in data]           # Recurse into lists
                 if isinstance(data, OrderedDict):
                     return {"py/collections.OrderedDict":
-                            [[serialize_recursively(k), serialize_recursively(v)] for k, v in data.iteritems()]}
-                if _isnamedtuple(data):
+                            [[serializeRecursively(k), serializeRecursively(v)] for k, v in data.iteritems()]}
+                if _isNamedTuple(data):
                     return {"py/collections.namedtuple": {
                         "type":   type(data).__name__,
                         "fields": list(data._fields),
-                        "values": [serialize_recursively(getattr(data, f)) for f in data._fields]}}
+                        "values": [serializeRecursively(getattr(data, f)) for f in data._fields]}}
                 if isinstance(data, dict):
                     if all(isinstance(k, basestring) for k in data):   # Recurse into dicts
-                        return {k: serialize_recursively(v) for k, v in data.iteritems()}
+                        return {k: serializeRecursively(v) for k, v in data.iteritems()}
                     else:
-                        return {"py/dict": [[serialize_recursively(k), serialize_recursively(v)] for k, v in data.iteritems()]}
+                        return {"py/dict": [[serializeRecursively(k), serializeRecursively(v)] for k, v in data.iteritems()]}
                 if isinstance(data, tuple):                          # Recurse into tuples
-                    return {"py/tuple": [serialize_recursively(val) for val in data]}
+                    return {"py/tuple": [serializeRecursively(val) for val in data]}
                 if isinstance(data, set):                            # Recurse into sets
-                    return {"py/set": [serialize_recursively(val) for val in data]}
+                    return {"py/set": [serializeRecursively(val) for val in data]}
                 if isinstance(data, datetime.datetime):
                     return {"py/datetime": str(data)}
-                if isinstance(data, np.ndarray):
-                    if numpy_storage == 'ascii' or (numpy_storage == 'auto' and data.size < 1000):
+                if isinstance(data, ndarray):
+                    if numpyStorage == 'ascii' or (numpyStorage == 'auto' and data.size < 1000):
                         return {"py/numpy.ndarray.ascii": {
                             "shape": data.shape,
                             "values": data.tolist(),
                             "dtype":  str(data.dtype)}}
                     else:
-                        import base64
-                    return {"py/numpy.ndarray.base64": {
+                        from base64 import b64encode
+                        return {"py/numpy.ndarray.base64": {
                         "shape": data.shape,
-                        "values": base64.b64encode(data),
+                        "values": b64encode(data),
                         "dtype":  str(data.dtype)}}
 
                 raise TypeError("Type %s not data-serializable" % type(data))
 
             # Start serializing from the top level object dictionary
-            return serialize_recursively(self.wrapped.__dict__)
+            return serializeRecursively(self.wrapped.__dict__)
 
         # ------------------------------------------------------------------------------
         # DESERIALIZE()
 
         @staticmethod
-        def deserialize(serialized_dict):
+        def deserialize(serializedDict):
             '''
             Restore the object that has been converted to a python dictionary using an @serializable
             class's serialize() method.
 
             Arguments
 
-                serialized_dict: a python dictionary returned by serialize()
+                serializedDict: a python dictionary returned by serialize()
 
             Returns:
 
                 A reconstituted class instance
             '''
 
-            def restore_recursively(dct):
+            def restoreRecursively(dct):
                 '''
                 This object hook helps to deserialize object encoded using the
                 serialize() method above.
                 '''
-                import numpy as np
-                import base64
+                from numpy import frombuffer, dtype, array
+                from base64 import decodestring
 
                 if "py/dict" in dct:
-                    return dict(restore_recursively(dct["py/dict"]))
+                    return dict(restoreRecursively(dct["py/dict"]))
                 if "py/tuple" in dct:
-                    return tuple(restore_recursively(dct["py/tuple"]))
+                    return tuple(restoreRecursively(dct["py/tuple"]))
                 if "py/set" in dct:
-                    return set(restore_recursively(dct["py/set"]))
+                    return set(restoreRecursively(dct["py/set"]))
                 if "py/collections.namedtuple" in dct:
-                    data = restore_recursively(dct["py/collections.namedtuple"])
+                    data = restoreRecursively(dct["py/collections.namedtuple"])
                     return namedtuple(data["type"], data["fields"])(*data["values"])
                 if "py/collections.OrderedDict" in dct:
-                    return OrderedDict(restore_recursively(dct["py/collections.OrderedDict"]))
+                    return OrderedDict(restoreRecursively(dct["py/collections.OrderedDict"]))
                 if "py/datetime" in dct:
                     from dateutil import parser
                     return parser.parse(dct["py/datetime"])
                 if "py/numpy.ndarray.ascii" in dct:
                     data = dct["py/numpy.ndarray.ascii"]
-                    return np.array(data["values"], dtype=data["dtype"])
+                    return array(data["values"], dtype=data["dtype"])
                 if "py/numpy.ndarray.base64" in dct:
                     data = dct["py/numpy.ndarray.base64"]
-                    arr = np.frombuffer(base64.decodestring(data["values"]), np.dtype(data["dtype"]))
+                    arr = frombuffer(decodestring(data["values"]), dtype(data["dtype"]))
                     return arr.reshape(data["shape"])
 
                 # Base case: data type needs no further decoding.
@@ -208,24 +208,24 @@ def serializable(cls):
 
             # First we must restore the object's dictionary entries.  These are decoded recursively
             # using the helper function above.
-            restored_dict = {}
-            for k in serialized_dict.keys():
-                restored_dict[k] = restore_recursively(serialized_dict[k])
+            restoredDict = {}
+            for k in serializedDict.keys():
+                restoredDict[k] = restoreRecursively(serializedDict[k])
 
             # Next we recreate the object. Calling the __new__() function here creates
             # an empty object without calling __init__(). We then take this empty
             # shell of an object, and set its dictionary to the reconstructed
             # dictionary we pulled from the JSON file.
-            thawed_object = cls.__new__(cls)
-            thawed_object.__dict__ = restored_dict
+            thawedObject = cls.__new__(cls)
+            thawedObject.__dict__ = restoredDict
 
             # Finally, we would like this re-hydrated object to also be @serializable, so we re-wrap it
             # in the ThunderSerializeableObjectWrapper using the same trick with __new__().
-            rewrapped_object = ThunderSerializeableObjectWrapper.__new__(ThunderSerializeableObjectWrapper)
-            rewrapped_object.__dict__['wrapped'] = thawed_object
+            rewrappedObject = ThunderSerializeableObjectWrapper.__new__(ThunderSerializeableObjectWrapper)
+            rewrappedObject.__dict__['wrapped'] = thawedObject
 
             # Return the re-constituted class
-            return rewrapped_object
+            return rewrappedObject
 
     # End of decorator.  Return the wrapper class from inside this closure.
     return ThunderSerializeableObjectWrapper
