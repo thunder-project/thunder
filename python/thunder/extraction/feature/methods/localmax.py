8aca2b89c4fa3be30e271f9c1ea98625ad2cee70
freeman <the.freeman.lab@gmail.com>
2015-03-21 18:25:09 -0400

    Add a local max algorithm using scikit-image

--- python/thunder/extraction/feature/methods/localmax.py
+++ python/thunder/extraction/feature/methods/localmax.py
@@ -1,5 +1,7 @@
 from thunder.extraction.feature.base import FeatureMethod, FeatureAlgorithm
 from thunder.extraction.feature.preprocessors import MeanPreprocessor
+from thunder.extraction.source import SourceModel, Source
+
 
 class LocalMax(FeatureMethod):
 
@@ -11,8 +13,20 @@ class LocalMax(FeatureMethod):
 
 class LocalMaxAlgorithm(FeatureAlgorithm):
 
-    def __init__(self, threshold=10, **extra):
-        self.threshold = threshold
+    def __init__(self, min_distance=10, num_peaks=None, **extra):
+        self.min_distance = min_distance
+        self.num_peaks = num_peaks
 
     def extract(self, im):
-        pass
+        from numpy import ones, concatenate
+        from skimage.feature import peak_local_max
+
+        if im.ndim == 2:
+            peaks = peak_local_max(im, min_distance=self.min_distance, num_peaks=self.num_peaks).tolist()
+        else:
+            peaks = []
+            for i in range(0, im.shape[2]):
+                tmp = peak_local_max(im[:, :, i], min_distance=self.min_distance, num_peaks=self.num_peaks)
+                peaks = peaks.append(concatenate((tmp, ones((len(tmp), 1)) * i), axis=1))
+
+        return SourceModel([Source([p]) for p in peaks])
