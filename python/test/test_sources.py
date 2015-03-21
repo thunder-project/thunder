e25d6effdaee1ab8777cbe7efc5041b9bc44fa46
freeman <the.freeman.lab@gmail.com>
2015-03-21 18:25:37 -0400

    Sketch of tests

---
+++ python/test/test_sources.py
@@ -0,0 +1,13 @@
+
+
+# input with one coordinate and one value
+
+# input with multiple coordinates
+
+# correctly compute center and bounding
+
+# conversion to array
+
+# conversion to list
+
+# creation of a source model
