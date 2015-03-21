1e9b1e940333ae4843a9a88d5acb026864c0f54d
freeman <the.freeman.lab@gmail.com>
2015-03-21 18:58:07 -0400

    Convert local max points to circles

--- python/thunder/extraction/feature/methods/localmax.py
+++ python/thunder/extraction/feature/methods/localmax.py
@@ -1,3 +1,5 @@
+from numpy import cos, sin, pi
+
 from thunder.extraction.feature.base import FeatureMethod, FeatureAlgorithm
 from thunder.extraction.feature.preprocessors import MeanPreprocessor
 from thunder.extraction.source import SourceModel, Source
@@ -13,20 +15,25 @@ class LocalMax(FeatureMethod):
 
 class LocalMaxAlgorithm(FeatureAlgorithm):
 
-    def __init__(self, min_distance=10, num_peaks=None, **extra):
-        self.min_distance = min_distance
-        self.num_peaks = num_peaks
+    def __init__(self, minDistance=10, numPeaks=None, **extra):
+        self.minDistance = minDistance
+        self.numPeaks = numPeaks
 
-    def extract(self, im):
+    def extract(self, im, radius=5, res=10):
         from numpy import ones, concatenate
         from skimage.feature import peak_local_max
 
         if im.ndim == 2:
-            peaks = peak_local_max(im, min_distance=self.min_distance, num_peaks=self.num_peaks).tolist()
+            peaks = peak_local_max(im, min_distance=self.minDistance, num_peaks=self.numPeaks).tolist()
         else:
             peaks = []
             for i in range(0, im.shape[2]):
-                tmp = peak_local_max(im[:, :, i], min_distance=self.min_distance, num_peaks=self.num_peaks)
+                tmp = peak_local_max(im[:, :, i], min_distance=self.minDistance, num_peaks=self.numPeaks)
                 peaks = peaks.append(concatenate((tmp, ones((len(tmp), 1)) * i), axis=1))
 
-        return SourceModel([Source([p]) for p in peaks])
+        def pointToCircle(center):
+            xy = [(cos(2 * pi/res * x) * radius + center[0], sin(2 * pi/res * x) * radius + center[1])
+                  for x in xrange(0, res+1)]
+            return xy
+
+        return SourceModel([Source(pointToCircle(p)) for p in peaks])
