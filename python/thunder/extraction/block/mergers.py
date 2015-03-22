2fe9a1715daa33ae8896947164a81dc3106aa2cc
freeman <the.freeman.lab@gmail.com>
2015-03-22 18:01:53 -0400

    Add NMF and a basic block merger

--- python/thunder/extraction/block/mergers.py
+++ python/thunder/extraction/block/mergers.py
@@ -1,4 +1,7 @@
+from numpy import asarray, size
+
 from thunder.extraction.block.base import BlockMerger
+from thunder.extraction.source import SourceModel
 
 
 class BasicBlockMerger(BlockMerger):
@@ -6,5 +9,15 @@ class BasicBlockMerger(BlockMerger):
     def __init__(self, overlap=0.5, **extra):
         self.overlap = overlap
 
-    def merge(self, sources, data=None):
-        pass
+    def merge(self, sources, keys, data=None):
+        import itertools
+
+        # recenter coordinates using the spatial key from each block
+        for (i, sourceBlock) in enumerate(sources):
+            for source in sourceBlock:
+                source.coordinates += keys[i].spatialKey
+
+        # flatten list of sources
+        chain = itertools.chain.from_iterable(sources)
+        sources = list(chain)
+        return SourceModel(sources)
--- python/thunder/extraction/block/methods/nmf.py
+++ python/thunder/extraction/block/methods/nmf.py
@@ -1,4 +1,5 @@
 from thunder.extraction.block.base import BlockMethod, BlockAlgorithm
+from thunder.extraction.source import Source
 
 
 class BlockNMF(BlockMethod):
@@ -9,9 +10,65 @@ class BlockNMF(BlockMethod):
 
 
 class NMFBlockAlgorithm(BlockAlgorithm):
+    """
+    Find sources using non-negative matrix factorization on blocks.
 
-    def __init__(self, maxIter=10, **extra):
+    NMF on each block provides a candidate set of basis functions.
+    These are then converted into regions using simple morphological operators:
+    labeling connected components, and removing all that fail to meet
+    min and max size thresholds.
+
+    Parameters
+    ----------
+    maxIter : int, optional, default = 10
+        Maximum number of iterations
+
+    componentsPerBlock : int, optional, deafut = 3
+        Number of components to find per block
+    """
+    def __init__(self, maxIter=10, componentsPerBlock=3, percentile=75, minArea=50, maxArea="block", **extra):
         self.maxIter = maxIter
+        self.componentsPerBlock = componentsPerBlock
+        self.percentile = percentile
+        self.minArea = minArea
+        self.maxArea = maxArea
 
     def extract(self, block):
-        pass
+        from numpy import clip, inf, percentile, asarray, where, size, prod
+        from sklearn.decomposition import NMF
+        from skimage.measure import label
+        from skimage.morphology import remove_small_objects
+
+        # get dimensions
+        n = self.componentsPerBlock
+        dims = block.shape[1:]
+
+        # handle maximum size
+        if self.maxArea == "block":
+            maxArea = prod(dims) / 2
+        else:
+            maxArea = self.maxArea
+
+        # reshape to be t x all spatial dimensions
+        data = block.reshape(block.shape[0], -1)
+
+        # build and apply NMF model to block
+        model = NMF(n, max_iter=self.maxIter)
+        model.fit(clip(data, 0, inf))
+
+        # reconstruct sources as spatial objects in one array
+        comps = model.components_.reshape((n,) + dims)
+
+        # convert from basis functions into shape
+        # by finding connected components and removing small objects
+        combined = []
+        for c in comps:
+            tmp = c > percentile(c, self.percentile)
+            shape = remove_small_objects(label(tmp), min_size=self.minArea)
+            coords = asarray(where(shape)).T
+            if (size(coords) > 0) and (size(coords) < maxArea):
+                combined.append(Source(coords))
+
+        return combined
+
+
