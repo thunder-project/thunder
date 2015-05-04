8ccf60740769a5877af667067c801c1a16741de1
freeman <the.freeman.lab@gmail.com>
2015-05-04 10:37:17 -0400

    Variable name change

--- python/thunder/rdds/imgblocks/blocks.py
+++ python/thunder/rdds/imgblocks/blocks.py
@@ -425,9 +425,9 @@ class PaddedBlockGroupingKey(BlockGroupingKey):
 
     @property
     def padding(self):
-        start = [self.imgSlices[i].start - self.padImgSlices[i].start for i in range(1, len(self.origShape))]
-        stop = [self.padImgSlices[i].stop - self.imgSlices[i].stop for i in range(1, len(self.origShape))]
-        return start, stop
+        before = [self.imgSlices[i].start - self.padImgSlices[i].start for i in range(1, len(self.origShape))]
+        after = [self.padImgSlices[i].stop - self.imgSlices[i].stop for i in range(1, len(self.origShape))]
+        return before, after
 
     def asTemporallyConcatenatedKey(self):
         """Returns a new key that is a copy of self, except that the new temporal range is from 0 to the total number
