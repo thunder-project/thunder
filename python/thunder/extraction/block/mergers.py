e455fea320a5b1a43b32d8c2fbafad655f7047da
freeman <the.freeman.lab@gmail.com>
2015-05-18 14:12:13 -0400

    Docs and renaming

--- python/thunder/extraction/block/mergers.py
+++ python/thunder/extraction/block/mergers.py
@@ -3,24 +3,42 @@ from thunder.extraction.source import SourceModel
 
 
 class BasicBlockMerger(BlockMerger):
-
+    """
+    Simple merger that combines sources directly
+    without any merging or modification
+    """
     def __init__(self, **extra):
         pass
 
-    def merge(self, blocks, keys, data=None):
+    def merge(self, blocks, keys):
+        """
+        Parameters
+        ----------
+        blocks : list of lists of sources
+            List of the sources found for each block; every block
+            should be represented, with an empty list for blocks
+            without any identified sources
+
+        keys : List of BlockGroupingKeys or PaddedBlockGroupingKeys
+            The keys for each of the blocks assocaited with the sources
+
+        Returns
+        -------
+        SourceModel containing the combined list of sources
+        """
         import itertools
         from thunder.rdds.imgblocks.blocks import PaddedBlockGroupingKey
 
         # recenter coordinates using the spatial key from each block
         # also subtract off initial padding if blocks were padded
-        for i, blk in enumerate(blocks):
+        for ib, blk in enumerate(blocks):
 
             for source in blk:
-                source.coordinates += keys[i].spatialKey
+                source.coordinates += keys[ib].spatialKey
 
             if isinstance(keys[0], PaddedBlockGroupingKey):
                 for source in blk:
-                    source.coordinates -= keys[i].padding[0]
+                    source.coordinates -= keys[ib].padding[0]
 
         # flatten list of sources
         chain = itertools.chain.from_iterable(blocks)
