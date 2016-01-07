ea606ddc63afca70df670360b4ae70ff102416aa
freeman <the.freeman.lab@gmail.com>
2016-01-07 17:03:43 -0500

    Fix setup

--- setup.py
+++ setup.py
@@ -5,11 +5,7 @@ from setuptools import setup
 version = '0.6.0.dev'
 
 required = open('requirements.txt').read().split()
-extra = {
-    'mist': ['mist'],
-    'rime': ['rime']
-}
-extra['all'] = sorted(set(sum(extra.values(), [])))
+extra = {'all': ['mist', 'rime']}
 
 setup(
     name='thunder-python',
@@ -20,15 +16,10 @@ setup(
     url='https://github.com/thunder-project/thunder',
     packages=[
         'thunder',
-        'thunder.clustering',
-        'thunder.factorization',
-        'thunder.data',
-        'thunder.data.blocks',
-        'thunder.data.series',
-        'thunder.data.images',
-        'thunder.regression',
-        'thunder.regression.linear',
-        'thunder.regression.nonlinear'
+        'thunder',
+        'thunder.blocks',
+        'thunder.series',
+        'thunder.images'
     ],
     package_data={'thunder.lib': ['thunder_python-' + version + '-py2.7.egg']},
     install_requires=required,
