1e618cc4bbec135afea5ba66cf9918963206c5ed
freeman <the.freeman.lab@gmail.com>
2016-01-12 14:49:57 -0500

    Remove plane and crop tests

--- test/test_images.py
+++ test/test_images.py
@@ -152,19 +152,6 @@ def test_var(eng):
     assert allclose(data.var().toarray(), original.var(axis=0))
 
 
-def test_crop(eng):
-    original = arange(24).reshape((4, 6))
-    data = fromlist([original], engine=eng)
-    assert allclose(data.crop((0, 0), (2, 2)).toarray(), original[0:2, 0:2])
-    assert allclose(data.crop((0, 1), (2, 2)).toarray(), original[0:2, 1:2])
-
-
-def test_planes(eng):
-    original = arange(24).reshape((2, 3, 4))
-    data = fromlist([original], engine=eng)
-    assert allclose(data.planes(0, 1).toarray(), original[:, :, 0:1])
-
-
 def test_subtract(eng):
     original = arange(24).reshape((4, 6))
     data = fromlist([original], engine=eng)
