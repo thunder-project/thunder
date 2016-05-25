9c865520852d62c8dbc41850989a7c0b948254e8
Mohar, Boaz <moharb@janelia.hhmi.org>
2016-05-24 22:22:58 -0400

    Added check for series as well

--- test/test_base.py
+++ test/test_base.py
@@ -113,3 +113,11 @@ def test_repartition(eng):
         assert allclose(data.first(), array([1, 1]))
         data = data.repartition(3)
         assert allclose(data.first(), array([1, 1]))
+
+        data = series.fromlist([array([1, 1]), array([2, 2]), array([3, 3]), array([4, 4]),
+                                array([5, 5]), array([6, 6]), array([7, 7]), array([8, 8]),
+                                array([9, 9]), array([10, 10]), array([11, 11]), array([12, 12])],
+                               engine=eng, npartitions=10)
+        assert allclose(data.first(), array([1, 1]))
+        data = data.repartition(3)
+        assert allclose(data.first(), array([1, 1]))
